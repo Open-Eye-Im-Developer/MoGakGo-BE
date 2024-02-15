dc4f96041ca2888f34e8d237918463eb1bbd31af
KyungMin Lee <hello.tidavid@gmail.com>
2024-02-15 18:56:31 +0900

    [FEAT] DevelopLanguage 추가

--- src/main/java/io/oeid/mogakgo/domain/user/domain/enums/DevelopLanguage.java
+++ src/main/java/io/oeid/mogakgo/domain/user/domain/enums/DevelopLanguage.java
@@ -1,4 +1,5 @@
 package io.oeid.mogakgo.domain.user.domain.enums;
 
 public enum DevelopLanguage {
+    PYTHON, JAVA, JAVASCRIPT, C, CPP, CSHARP, RUBY, SWIFT, KOTLIN, GO, TYPESCRIPT, SCALA, RUST, PHP, HTML, CSS, ELM, ERLANG, HASKELL, R, SHELL, SQL, DART, OBJECT_C, ETC;
 }
