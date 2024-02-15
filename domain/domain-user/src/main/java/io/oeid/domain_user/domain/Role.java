50ca28a654e58e2e12e4c86553f29c578da605a2
KyungMin Lee <hello.tidavid@gmail.com>
2024-02-15 09:53:51 +0900

    [REFACT] 코어 및 도메인 모듈 제거

--- domain/domain-user/src/main/java/io/oeid/domain_user/domain/Role.java
+++
@@ -1,5 +0,0 @@
-package io.oeid.domain_user.domain;
-
-public enum Role {
-    ROLE_USER
-}
---
+++ src/main/java/io/oeid/mogakgo/MogakGoApplication.java
@@ -0,0 +1,13 @@
+package io.oeid.mogakgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MogakGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MogakGoApplication.class, args);
+    }
+
+}
