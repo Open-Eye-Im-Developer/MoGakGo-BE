977524757dc7a76f65fbfe483ea156e552a36801
JIN-076 <andy230@naver.com>
2024-02-18 20:00:51 +0900

    [FEAT] 동네 인증을 요청한 사용자와 토큰에 대한 유효성 검사 로직, 동네 인증을 요청한 법정구역코드에 대한 유효성 검사 로직, 동네 인증 요청 처리 로직 구현

---
+++ src/main/java/io/oeid/mogakgo/domain/cert/application/CertService.java
@@ -0,0 +1,56 @@
+package io.oeid.mogakgo.domain.cert.application;
+
+import static io.oeid.mogakgo.exception.code.ErrorCode400.INVALID_SERVICE_REGION;
+import static io.oeid.mogakgo.exception.code.ErrorCode401.CERT_INVALID_INFORMATION;
+
+import io.oeid.mogakgo.domain.cert.exception.CertException;
+import io.oeid.mogakgo.domain.geo.domain.enums.Region;
+import io.oeid.mogakgo.domain.geo.exception.GeoException;
+import io.oeid.mogakgo.domain.user.application.UserCommonService;
+import io.oeid.mogakgo.domain.user.application.UserGeoService;
+import io.oeid.mogakgo.domain.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CertService {
+
+    private final UserGeoService userGeoService;
+    private final UserCommonService userCommonService;
+
+    public Long certificate(Long tokenUserId, Long userId, int areaCode) {
+        User tokenUser = validateToken(tokenUserId);
+        validateCertificator(tokenUser, userId);
+        Region region = validateAreaCodeCoverage(areaCode);
+        if (isPossibleCertification(userId, region)) {
+            userGeoService.updateUserGeo(userId, region);
+        }
+        return userId;
+    }
+
+    private User validateToken(Long userId) {
+        return userCommonService.getUserById(userId);
+    }
+
+    // 사용자가 아직 동네 인증을 하지 않았거나, 새롭게 인증하려는 지역이 이미 인증된 지역과 다를 경우만 동네 인증 처리
+    private boolean isPossibleCertification(Long userId, Region region) {
+        Region userRegionInfo = userGeoService.getUserGeo(userId).getRegion();
+        return userRegionInfo == null || userRegionInfo != region;
+    }
+
+    private void validateCertificator(User tokenUser, Long userId) {
+        if (!tokenUser.getId().equals(userId)) {
+            throw new CertException(CERT_INVALID_INFORMATION);
+        }
+    }
+
+    private Region validateAreaCodeCoverage(int areaCode) {
+        Region region = Region.getByAreaCode(areaCode);
+        if (region == null) {
+            throw new GeoException(INVALID_SERVICE_REGION);
+        }
+        return region;
+    }
+
+}
