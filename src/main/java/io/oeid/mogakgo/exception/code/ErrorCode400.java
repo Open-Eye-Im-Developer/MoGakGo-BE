6bf9938363e9fa44294c3fe3c4c6c5e5b971f4e2
KyungMin Lee <hello.tidavid@gmail.com>
2024-02-18 13:39:59 +0900

    [FEAT] FCM Token VO 기능 구현

--- src/main/java/io/oeid/mogakgo/exception/code/ErrorCode400.java
+++ src/main/java/io/oeid/mogakgo/exception/code/ErrorCode400.java
@@ -10,6 +10,7 @@ public enum ErrorCode400 implements ErrorCode {
 
     NOTIFICATION_TAG_NOT_NULL("E060001", "알림 태그는 필수값입니다."),
     NOTIFICATION_DETAIL_DATA_NOT_NULL("E060002", "알림 상세 데이터는 필수값입니다."),
+    NOTIFICATION_FCM_TOKEN_NOT_NULL("E060003", "FCM 토큰은 필수값입니다."),
 
     INVALID_PROJECT_MEETING_TIME("E030101", "프로젝트 만남 시간이 유효하지 않습니다."),
     INVALID_PROJECT_TAG_COUNT("E030102", "프로젝트 태그 갯수가 유효하지 않습니다. 1개 이상 3개 이하로 입력해야 합니다."),
@@ -22,6 +23,7 @@ public enum ErrorCode400 implements ErrorCode {
     USER_WANTED_JOB_BAD_REQUEST("E020102", "희망 직무는 3개까지만 등록 가능합니다."),
     USERNAME_SHOULD_BE_NOT_EMPTY("E020103", "유저 이름은 비어있을 수 없습니다."),
     USER_REGION_SHOULD_BE_NOT_EMPTY("E020104", "유저 지역은 비어있을 수 없습니다."),
+    USER_ID_NOT_NULL("E020001", "유저 아이디는 필수값입니다."),
     ;
 
     private final HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
