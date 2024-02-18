9aad03ea01b0929688eeb6d89b26c68a94cb7063
KyungMin Lee <hello.tidavid@gmail.com>
2024-02-18 13:30:51 +0900

    Refact/#64 user domain (#67)
    * [FEAT] 회원가입 여부 전달 기능 구현
    
    * [FEAT] .ignore Update
    
    * [FEAT] geoService 기능 추가
    
    * [FEAT] 서버 URL 등록

--- src/main/java/io/oeid/mogakgo/domain/auth/presentation/AuthController.java
+++ src/main/java/io/oeid/mogakgo/domain/auth/presentation/AuthController.java
@@ -5,6 +5,7 @@ import io.oeid.mogakgo.domain.auth.presentation.dto.res.AuthLoginUrlResponse;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -17,23 +18,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/oauth")
 public class AuthController {
 
+    private final String serverUrl;
+
+    public AuthController(@Value("${auth.server-url}") String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+
     @GetMapping("/login/success")
     public ResponseEntity<AuthAccessTokenResponse> loginSuccess(
         @AuthenticationPrincipal OAuth2User oAuth2User, HttpServletResponse response) {
         String accessToken = oAuth2User.getAttributes().get("accessToken").toString();
         String refreshToken = oAuth2User.getAttributes().get("refreshToken").toString();
         int refreshTokenExpireTime = (int) oAuth2User.getAttributes().get("refreshTokenExpireTime");
+        boolean signUpComplete = (boolean) oAuth2User.getAttributes().get("signUpComplete");
         setCookie(refreshToken, refreshTokenExpireTime, response);
-        return ResponseEntity.ok(AuthAccessTokenResponse.from(accessToken));
+        return ResponseEntity.ok(AuthAccessTokenResponse.of(accessToken, signUpComplete));
     }
 
     @GetMapping("/login")
     public ResponseEntity<AuthLoginUrlResponse> login() {
         return ResponseEntity.ok(
-            AuthLoginUrlResponse.from("http://127.0.0.1:8080/oauth2/authorization/github"));
+            AuthLoginUrlResponse.from(serverUrl + "/oauth2/authorization/github"));
     }
 
-    private void setCookie(String refreshToken, int refreshTokenExpireTime, HttpServletResponse response) {
+    private void setCookie(String refreshToken, int refreshTokenExpireTime,
+        HttpServletResponse response) {
         Cookie cookie = new Cookie("refreshToken", refreshToken);
         cookie.setMaxAge(refreshTokenExpireTime);
         cookie.setPath("/");
