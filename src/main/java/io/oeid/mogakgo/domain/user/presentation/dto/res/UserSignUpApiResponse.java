b573722e63a703a6873c33c96586aa366da23893
KyungMin Lee <hello.tidavid@gmail.com>
2024-02-19 00:58:22 +0900

    [DOCS] User API Swagger 추가

--- src/main/java/io/oeid/mogakgo/domain/user/presentation/dto/res/UserSignUpApiResponse.java
+++ src/main/java/io/oeid/mogakgo/domain/user/presentation/dto/res/UserSignUpApiResponse.java
@@ -1,21 +1,30 @@
 package io.oeid.mogakgo.domain.user.presentation.dto.res;
 
 import io.oeid.mogakgo.domain.user.application.dto.res.UserSignUpResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@Schema(description = "회원 가입 응답")
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserSignUpApiResponse {
 
+    @Schema(description = "회원 ID", example = "1")
     private Long userId;
+    @Schema(description = "회원 이름", example = "거루")
     private String username;
+    @Schema(description = "깃허브 ID", example = "tidavid1")
     private String githubId;
+    @Schema(description = "회원 프로필 이미지 URL", example = "https://avatars.githubusercontent.com/u/85854384?v=4")
     private String avatarUrl;
+    @Schema(description = "깃허브 URL", example = "https://github.com/tidavid1")
     private String githubUrl;
+    @Schema(description = "개발 언어", example = "[\"JAVA\", \"KOTLIN\"]")
     private List<String> developLanguages;
+    @Schema(description = "희망 직무", example = "[\"BACKEND\", \"FRONTEND\"]")
     private List<String> wantedJobs;
 
     public static UserSignUpApiResponse from(UserSignUpResponse response) {
