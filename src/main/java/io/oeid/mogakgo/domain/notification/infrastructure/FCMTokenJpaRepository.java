294297e05cb6e7d6ced64cebb168815d68dd8796
KyungMin Lee <hello.tidavid@gmail.com>
2024-02-18 13:47:19 +0900

    [FEAT] FCM Token Repository 구현

--- src/main/java/io/oeid/mogakgo/domain/notification/infrastructure/FCMTokenJpaRepository.java
+++ src/main/java/io/oeid/mogakgo/domain/notification/infrastructure/FCMTokenJpaRepository.java
@@ -1,10 +1,10 @@
 package io.oeid.mogakgo.domain.notification.infrastructure;
 
-import io.oeid.mogakgo.domain.notification.domain.Notification;
+import io.oeid.mogakgo.domain.notification.domain.vo.FCMToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NotificationJpaRepository extends JpaRepository<Notification, Long> {
+public interface FCMTokenJpaRepository extends JpaRepository<FCMToken, Long> {
 
 }
