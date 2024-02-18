9d6340743848c8f13757a9ef510e69519575c382
KyungMin Lee <hello.tidavid@gmail.com>
2024-02-18 23:00:53 +0900

    [FEAT] AuthLoginUrlResponse `@Schema` 추가

--- src/main/java/io/oeid/mogakgo/domain/auth/presentation/dto/res/AuthLoginUrlResponse.java
+++ src/main/java/io/oeid/mogakgo/domain/auth/presentation/dto/res/AuthLoginUrlResponse.java
@@ -1,10 +1,13 @@
 package io.oeid.mogakgo.domain.auth.presentation.dto.res;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
+@Schema(description = "로그인 URL 반환 응답")
 @Getter
 public class AuthLoginUrlResponse {
 
+    @Schema(description = "로그인 URL", example = "http://3.38.76.76:8080/oauth2/authorization/github")
     private final String loginUrl;
 
     private AuthLoginUrlResponse(String loginUrl) {
