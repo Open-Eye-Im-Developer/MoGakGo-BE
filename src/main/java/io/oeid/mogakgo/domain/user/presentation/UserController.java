1708434422c3dc66cab078d7414c05205392c7ab
KyungMin Lee <hello.tidavid@gmail.com>
2024-02-19 00:44:23 +0900

    [REMOVE] 사용하지 않는 API 삭제

--- src/main/java/io/oeid/mogakgo/domain/user/presentation/UserController.java
+++ src/main/java/io/oeid/mogakgo/domain/user/presentation/UserController.java
@@ -3,16 +3,13 @@ package io.oeid.mogakgo.domain.user.presentation;
 import io.oeid.mogakgo.common.annotation.UserId;
 import io.oeid.mogakgo.domain.user.application.UserService;
 import io.oeid.mogakgo.domain.user.presentation.dto.req.UserSignUpApiRequest;
-import io.oeid.mogakgo.domain.user.presentation.dto.res.UserProfileApiResponse;
 import io.oeid.mogakgo.domain.user.presentation.dto.res.UserSignUpApiResponse;
 import jakarta.annotation.Nullable;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,12 +26,6 @@ public class UserController {
         @RequestBody @Valid UserSignUpApiRequest apiRequest) {
         var response = userService.userSignUp(apiRequest.toRequest(userId));
         return ResponseEntity.ok(UserSignUpApiResponse.from(response));
-    }
-
-    @GetMapping("/{userId}")
-    public ResponseEntity<UserProfileApiResponse> userProfileApi(@PathVariable Long userId) {
-        var response = userService.getUserProfile(userId);
-        return ResponseEntity.ok(UserProfileApiResponse.from(response));
     }
 
     @DeleteMapping
