544dc670dfc6f7a4a155669fdb6f2df4da833f7d
JIN-076 <andy230@naver.com>
2024-02-18 20:08:54 +0900

    [FEAT] 사용자의 GPS 좌표에 대한 코드 요청, 서비스 지역에 대한 동네 인증 요청 API 구현

---
+++ src/main/java/io/oeid/mogakgo/domain/cert/presentation/CertController.java
@@ -0,0 +1,43 @@
+package io.oeid.mogakgo.domain.cert.presentation;
+
+import io.oeid.mogakgo.common.annotation.UserId;
+import io.oeid.mogakgo.domain.cert.application.CertService;
+import io.oeid.mogakgo.domain.cert.presentation.dto.req.UserRegionCertAPIReq;
+import io.oeid.mogakgo.domain.cert.presentation.dto.req.UserRegionInfoAPIReq;
+import io.oeid.mogakgo.domain.cert.presentation.dto.res.UserRegionCertAPIRes;
+import io.oeid.mogakgo.domain.cert.presentation.dto.res.UserRegionInfoAPIRes;
+import io.oeid.mogakgo.domain.geo.application.GeoService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api/v1/cert")
+@RestController
+@RequiredArgsConstructor
+public class CertController {
+
+    private final CertService certService;
+    private final GeoService geoService;
+
+    @PostMapping("/areacode")
+    public ResponseEntity<UserRegionInfoAPIRes> getUseRegionInfoByGPS(
+        @UserId Long userId, @Valid @RequestBody UserRegionInfoAPIReq request
+    ) {
+        int areaCode = geoService.getUserRegionInfoAboutCoordinates(
+            userId, request.getUserId(), request.getLongitude(), request.getLatitude()
+        );
+        return ResponseEntity.ok(UserRegionInfoAPIRes.from(areaCode));
+    }
+
+    @PostMapping("/certificate")
+    public ResponseEntity<UserRegionCertAPIRes> certificateNeighborhood(
+        @UserId Long userId, @Valid @RequestBody UserRegionCertAPIReq request
+    ) {
+        Long id = certService.certificate(userId, request.getUserId(), request.getAreaCode());
+        return ResponseEntity.ok(UserRegionCertAPIRes.from(id));
+    }
+}
