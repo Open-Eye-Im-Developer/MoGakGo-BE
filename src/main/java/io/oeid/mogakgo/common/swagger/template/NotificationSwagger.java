9ff940b9d7b2fce60c5a785d6ccfe0a40ecf94a3
KyungMin Lee <hello.tidavid@gmail.com>
2024-02-19 11:23:29 +0900

    Feat/#52 fcm token (#75)
    * [FEAT] FCM Token VO 기능 구현
    
    * [FEAT] FCM Token Repository 구현
    
    * [FEAT] FCM Token 저장 기능 구현
    
    * [FEAT] FCM Token 조회 로직 추가
    
    * [FEAT] FCM Notification Config 등록
    
    * [FEAT] FCM Notification 전송 기능 추가
    
    * [FEAT] Api 설명 문서 추가
    
    * [FEAT] Swagger Schema 추가 및 ErrorExamples 위치 수정

---
+++ src/main/java/io/oeid/mogakgo/common/swagger/template/NotificationSwagger.java
@@ -0,0 +1,32 @@
+package io.oeid.mogakgo.common.swagger.template;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+import io.oeid.mogakgo.core.properties.swagger.error.SwaggerUserErrorExamples;
+import io.oeid.mogakgo.domain.notification.presentation.dto.req.FCMTokenApiRequest;
+import io.oeid.mogakgo.exception.dto.ErrorResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+
+@Tag(name = "Notification", description = "알림 관련 API")
+@SuppressWarnings("unused")
+public interface NotificationSwagger {
+
+    @Operation(summary = "FCM 토큰 저장", description = "회원의 FCM 토큰을 저장할 때 사용하는 API")
+    @ApiResponse(responseCode = "200", description = "FCM 토큰 저장 성공")
+    @ApiResponse(responseCode = "404", description = "요청한 유저가 존재하지 않음", content = @Content(
+        mediaType = APPLICATION_JSON_VALUE,
+        schema = @Schema(implementation = ErrorResponse.class),
+        examples = {
+            @ExampleObject(name = "E020301", value = SwaggerUserErrorExamples.USER_NOT_FOUND)
+        })
+    )
+    ResponseEntity<Void> manageFCMToken(@Parameter(hidden = true) Long userId,
+        FCMTokenApiRequest request);
+}
