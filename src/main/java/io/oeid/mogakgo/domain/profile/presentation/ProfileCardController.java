58a5763401bd9663804be99bf4538273497aa2ff
JIN-076 <andy230@naver.com>
2024-02-19 12:36:22 +0900

    [FEAT] 프로필 카드 리스트 랜덤 조회 API 구현

---
+++ src/main/java/io/oeid/mogakgo/domain/profile/presentation/ProfileCardController.java
@@ -0,0 +1,35 @@
+package io.oeid.mogakgo.domain.profile.presentation;
+
+import io.oeid.mogakgo.common.annotation.UserId;
+import io.oeid.mogakgo.common.base.CursorPaginationInfoReq;
+import io.oeid.mogakgo.common.base.CursorPaginationResult;
+import io.oeid.mogakgo.common.swagger.template.ProfileCardSwagger;
+import io.oeid.mogakgo.domain.geo.domain.enums.Region;
+import io.oeid.mogakgo.domain.profile.application.ProfileCardService;
+import io.oeid.mogakgo.domain.user.application.dto.res.UserProfileResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/profile-cards")
+@RequiredArgsConstructor
+public class ProfileCardController implements ProfileCardSwagger {
+
+    private final ProfileCardService profileCardService;
+
+    @GetMapping("/{region}")
+    public ResponseEntity<CursorPaginationResult<UserProfileResponse>> getRandomOrderedProfileCardsByRegion(
+        @UserId Long userId, @PathVariable Region region,
+        @Valid @ModelAttribute CursorPaginationInfoReq pageable
+    ) {
+        return ResponseEntity.ok().body(
+            profileCardService.getRandomOrderedProfileCardsByRegion(userId, region, pageable)
+        );
+    }
+}
