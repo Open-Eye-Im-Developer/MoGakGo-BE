0e47fe4c587e755af92b14174e63615b6bcd17b2
JIN-076 <andy230@naver.com>
2024-02-19 20:37:19 +0900

    [REFACT] 읽기전용 @Transactional 추가

--- src/main/java/io/oeid/mogakgo/domain/profile/application/ProfileCardService.java
+++ src/main/java/io/oeid/mogakgo/domain/profile/application/ProfileCardService.java
@@ -13,8 +13,10 @@ import io.oeid.mogakgo.domain.user.presentation.dto.res.UserPublicApiResponse;
 import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ProfileCardService {
 
