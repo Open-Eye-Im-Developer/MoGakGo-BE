5ecec37973b620d6f6c9c389a0bcfea56d3d7867
KyungMin Lee <hello.tidavid@gmail.com>
2024-02-19 13:05:33 +0900

    [FEAT] 회원 가입시 유저 조회 기능 추가

--- src/main/java/io/oeid/mogakgo/domain/user/presentation/UserController.java
+++ src/main/java/io/oeid/mogakgo/domain/user/presentation/UserController.java
@@ -4,12 +4,13 @@ import io.oeid.mogakgo.common.annotation.UserId;
 import io.oeid.mogakgo.common.swagger.template.UserSwagger;
 import io.oeid.mogakgo.domain.user.application.UserService;
 import io.oeid.mogakgo.domain.user.presentation.dto.req.UserSignUpApiRequest;
+import io.oeid.mogakgo.domain.user.presentation.dto.res.UserProfileCardApiResponse;
 import io.oeid.mogakgo.domain.user.presentation.dto.res.UserSignUpApiResponse;
-import jakarta.annotation.Nullable;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,8 +23,14 @@ public class UserController implements UserSwagger {
 
     private final UserService userService;
 
+    @GetMapping
+    public ResponseEntity<UserProfileCardApiResponse> userGetApi(@UserId Long userId) {
+        var response = userService.getUserProfile(userId);
+        return ResponseEntity.ok(UserProfileCardApiResponse.from(response));
+    }
+
     @PatchMapping("/sign")
-    public ResponseEntity<UserSignUpApiResponse> userSignUpApi(@UserId @Nullable Long userId,
+    public ResponseEntity<UserSignUpApiResponse> userSignUpApi(@UserId Long userId,
         @RequestBody @Valid UserSignUpApiRequest apiRequest) {
         var response = userService.userSignUp(apiRequest.toRequest(userId));
         return ResponseEntity.ok(UserSignUpApiResponse.from(response));
