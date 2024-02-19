769c110bf743b1c72f2aa573ca979b03510af57c
JIN-076 <andy230@naver.com>
2024-02-20 00:11:19 +0900

    [REFACT] ProjectJoinRequest 엔티티의 Controller 엔드포인트 수정

--- src/main/java/io/oeid/mogakgo/domain/project_join_req/presentation/ProjectJoinRequestController.java
+++ src/main/java/io/oeid/mogakgo/domain/project_join_req/presentation/ProjectJoinRequestController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/project-join-request")
+@RequestMapping("/api/v1/projects/join-request")
 @RequiredArgsConstructor
 public class ProjectJoinRequestController implements ProjectJoinRequestSwagger {
 
