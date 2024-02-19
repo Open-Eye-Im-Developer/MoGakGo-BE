3bf22960060fd7529c6d81455753a5389dc25da9
JIN-076 <andy230@naver.com>
2024-02-19 12:37:25 +0900

    [FEAT] 프로필 카드 리스트 랜덤 조회 비즈니스 로직 구현, 커서 기반 페이지네이션 공통 포맷 적용

---
+++ src/main/java/io/oeid/mogakgo/domain/profile/application/ProfileCardService.java
@@ -0,0 +1,51 @@
+package io.oeid.mogakgo.domain.profile.application;
+
+import static io.oeid.mogakgo.exception.code.ErrorCode400.INVALID_SERVICE_REGION;
+import static io.oeid.mogakgo.exception.code.ErrorCode404.USER_NOT_FOUND;
+
+import io.oeid.mogakgo.common.base.CursorPaginationInfoReq;
+import io.oeid.mogakgo.common.base.CursorPaginationResult;
+import io.oeid.mogakgo.domain.geo.domain.enums.Region;
+import io.oeid.mogakgo.domain.geo.exception.GeoException;
+import io.oeid.mogakgo.domain.profile.infrastructure.ProfileCardJpaRepository;
+import io.oeid.mogakgo.domain.user.application.dto.res.UserProfileResponse;
+import io.oeid.mogakgo.domain.user.domain.User;
+import io.oeid.mogakgo.domain.user.exception.UserException;
+import io.oeid.mogakgo.domain.user.infrastructure.UserJpaRepository;
+import java.util.Collections;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProfileCardService {
+
+    private final ProfileCardJpaRepository profileCardRepository;
+    private final UserJpaRepository userRepository;
+
+    public CursorPaginationResult<UserProfileResponse> getRandomOrderedProfileCardsByRegion(
+        Long userId, Region region, CursorPaginationInfoReq pageable
+    ) {
+        getUser(userId);
+        validateRegionCoverage(region);
+
+        CursorPaginationResult<UserProfileResponse> projects = profileCardRepository
+            .findByConditionWithPagination(
+            null, region, null
+        );
+        Collections.shuffle(projects.getData());
+        return projects;
+    }
+
+    private User getUser(Long userId) {
+        return userRepository.findById(userId)
+            .orElseThrow(() -> new UserException(USER_NOT_FOUND));
+    }
+
+    private void validateRegionCoverage(Region region) {
+        if (Region.getByAreaCode(region.getAreaCode()) == null) {
+            throw new GeoException(INVALID_SERVICE_REGION);
+        }
+    }
+
+}
