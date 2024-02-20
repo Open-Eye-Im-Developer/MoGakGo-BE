826467e60fa17d704191c82566da077875ec8a0d
JIN-076 <andy230@naver.com>
2024-02-21 03:04:14 +0900

    [FEAT] userId를 이용해 프로필 카드를 조회하는 메서드 구현

--- src/main/java/io/oeid/mogakgo/domain/profile/infrastructure/ProfileCardJpaRepository.java
+++ src/main/java/io/oeid/mogakgo/domain/profile/infrastructure/ProfileCardJpaRepository.java
@@ -1,6 +1,7 @@
 package io.oeid.mogakgo.domain.profile.infrastructure;
 
 import io.oeid.mogakgo.domain.profile.domain.entity.ProfileCard;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,5 @@ import org.springframework.stereotype.Repository;
 public interface ProfileCardJpaRepository extends JpaRepository<ProfileCard, Long>,
     ProfileCardRepositoryCustom {
 
+    Optional<ProfileCard> findByUserId(Long userId);
 }
