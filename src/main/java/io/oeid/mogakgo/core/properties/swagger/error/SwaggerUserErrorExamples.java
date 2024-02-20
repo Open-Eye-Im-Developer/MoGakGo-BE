a735e249f7f924d6299f25d4dfef2e7e7cab3835
KyungMin Lee <hello.tidavid@gmail.com>
2024-02-21 00:46:16 +0900

    [FIX] 중복 로직 수정! (#121)

--- src/main/java/io/oeid/mogakgo/core/properties/swagger/error/SwaggerUserErrorExamples.java
+++ src/main/java/io/oeid/mogakgo/core/properties/swagger/error/SwaggerUserErrorExamples.java
@@ -3,6 +3,7 @@ package io.oeid.mogakgo.core.properties.swagger.error;
 public class SwaggerUserErrorExamples {
 
     public static final String USER_NOT_FOUND = "{\"timestamp\":\"2024-02-17T10:07:31.404Z\",\"statusCode\":404,\"code\":\"E020301\",\"message\":\"해당 유저가 존재하지 않습니다.\"}";
+    public static final String USER_WANTED_JOB_DUPLICATE = "{\"timestamp\":\"2024-02-17T10:07:31.404Z\",\"statusCode\":400,\"code\":\"E020105\",\"message\":\"중복된 희망 직무가 있습니다.\"}";
     public static final String INVALID_USER_NAME = "{\"timestamp\":\"2024-02-17T10:07:31.404Z\",\"statusCode\":400,\"code\":\"E020103\",\"message\":\"유저 이름은 비어있을 수 없습니다.\"}";
 
     private SwaggerUserErrorExamples() {
