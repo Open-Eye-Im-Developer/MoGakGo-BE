2f674139ba452220d9d1eb07f7e76d31f8e2f3fe
happyjamy <78072370+happyjamy@users.noreply.github.com>
2024-02-21 01:01:38 +0900

    [FIX] 매칭 생성 시 해당 프로젝트 다른 요청들 거절하는 로직 구현

--- src/main/java/io/oeid/mogakgo/domain/project_join_req/infrastructure/ProjectJoinRequestJpaRepository.java
+++ src/main/java/io/oeid/mogakgo/domain/project_join_req/infrastructure/ProjectJoinRequestJpaRepository.java
@@ -4,6 +4,7 @@ import io.oeid.mogakgo.domain.project_join_req.domain.entity.ProjectJoinRequest;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface ProjectJoinRequestJpaRepository extends JpaRepository<ProjectJoinRequest, Long>,
@@ -13,7 +14,6 @@ public interface ProjectJoinRequestJpaRepository extends JpaRepository<ProjectJo
     @Query("select pjr from ProjectJoinRequest pjr where pjr.id = :id")
     Optional<ProjectJoinRequest> findByIdWithProject(Long id);
 
-
     @Query("select pjr from ProjectJoinRequest pjr where pjr.sender.id = :senderId and pjr.requestStatus = 'PENDING'")
     Optional<ProjectJoinRequest> findPendingBySenderId(Long senderId);
 
@@ -22,5 +22,10 @@ public interface ProjectJoinRequestJpaRepository extends JpaRepository<ProjectJo
 
     @Query("select pjr from ProjectJoinRequest pjr where pjr.sender.id = :userId")
     Optional<ProjectJoinRequest> findAlreadyExistsAnotherJoinReq(Long userId);
+
+    @Modifying
+    @Query("update ProjectJoinRequest pjr set pjr.requestStatus = 'REJECTED' "
+        + "where pjr.project.id = :projectId and pjr.id != :acceptedRequestId and pjr.requestStatus = 'PENDING'")
+    int rejectNoAcceptedByProjectId(Long projectId, Long acceptedRequestId);
 
 }
