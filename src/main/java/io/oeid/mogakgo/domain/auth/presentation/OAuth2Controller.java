796a2e6c88f5bcb8ea5e48e5db11327afeb95318
KyungMin Lee <hello.tidavid@gmail.com>
2024-02-20 10:03:24 +0900

    [FIX] OAuth2 로그인 성공 페이지 반환 오류 수정

--- src/main/java/io/oeid/mogakgo/domain/auth/presentation/OAuth2Controller.java
+++ src/main/java/io/oeid/mogakgo/domain/auth/presentation/OAuth2Controller.java
@@ -1,10 +1,10 @@
 package io.oeid.mogakgo.domain.auth.presentation;
 
 import io.oeid.mogakgo.common.swagger.template.OAuth2Swagger;
-import io.oeid.mogakgo.domain.auth.presentation.dto.res.AuthAccessTokenResponse;
 import io.oeid.mogakgo.domain.auth.presentation.dto.res.AuthLoginUrlResponse;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -20,20 +20,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class OAuth2Controller implements OAuth2Swagger {
 
     private final String serverUrl;
+    private final String loginRedirectUrl;
 
-    public OAuth2Controller(@Value("${auth.server-url}") String serverUrl) {
+    public OAuth2Controller(@Value("${auth.server-url}") String serverUrl,
+        @Value("${auth.login-redirect-url}") String loginRedirectUrl) {
         this.serverUrl = serverUrl;
+        this.loginRedirectUrl = loginRedirectUrl;
     }
 
     @GetMapping("/login/success")
-    public ResponseEntity<AuthAccessTokenResponse> loginSuccess(
-        @AuthenticationPrincipal OAuth2User oAuth2User, HttpServletResponse response) {
+    public void loginSuccess(@AuthenticationPrincipal OAuth2User oAuth2User,
+        HttpServletResponse response) throws IOException {
         String accessToken = oAuth2User.getAttributes().get("accessToken").toString();
+        boolean signUpComplete = (boolean) oAuth2User.getAttributes().get("signUpComplete");
         String refreshToken = oAuth2User.getAttributes().get("refreshToken").toString();
         int refreshTokenExpireTime = (int) oAuth2User.getAttributes().get("refreshTokenExpireTime");
-        boolean signUpComplete = (boolean) oAuth2User.getAttributes().get("signUpComplete");
         setCookie(refreshToken, refreshTokenExpireTime, response);
-        return ResponseEntity.ok(AuthAccessTokenResponse.of(accessToken, signUpComplete));
+        response.sendRedirect(
+            loginRedirectUrl + "?accessToken=" + accessToken + "&signUpComplete=" + signUpComplete);
     }
 
     @GetMapping("/login")
