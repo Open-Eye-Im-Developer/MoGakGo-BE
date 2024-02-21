115308080549cc27e231c664e2f12d575c1c294f
JIN-076 <andy230@naver.com>
2024-02-21 09:20:17 +0900

    [FEAT] 읽기전용 @Transactional 어노테이션 추가

--- src/main/java/io/oeid/mogakgo/domain/profile/application/ProfileCardLikeService.java
+++ src/main/java/io/oeid/mogakgo/domain/profile/application/ProfileCardLikeService.java
@@ -18,6 +18,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ProfileCardLikeService {
 
