cb1b8f2319d1fc52963c8347984c6c31ee97f10f
KyungMin Lee <hello.tidavid@gmail.com>
2024-02-24 01:11:49 +0900

    [FEAT] 채팅방 종료시 접근할 수 없도록 로직 추가

--- src/main/java/io/oeid/mogakgo/domain/chat/application/ChatWebSocketService.java
+++ src/main/java/io/oeid/mogakgo/domain/chat/application/ChatWebSocketService.java
@@ -3,9 +3,11 @@ package io.oeid.mogakgo.domain.chat.application;
 
 import io.oeid.mogakgo.domain.chat.entity.document.ChatMessage;
 import io.oeid.mogakgo.domain.chat.entity.document.ChatRoom;
+import io.oeid.mogakgo.domain.chat.entity.enums.ChatStatus;
 import io.oeid.mogakgo.domain.chat.exception.ChatException;
 import io.oeid.mogakgo.domain.chat.infrastructure.ChatRepository;
 import io.oeid.mogakgo.domain.chat.infrastructure.ChatRoomRepository;
+import io.oeid.mogakgo.exception.code.ErrorCode400;
 import io.oeid.mogakgo.exception.code.ErrorCode404;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,7 +24,11 @@ public class ChatWebSocketService {
 
     @Transactional(readOnly = true)
     public ChatRoom findChatRoomById(String roomId) {
-        return chatRoomRepository.findById(roomId).orElseThrow(() -> new ChatException(ErrorCode404.CHAT_ROOM_NOT_FOUND));
+        ChatRoom chatRoom = chatRoomRepository.findById(roomId).orElseThrow(() -> new ChatException(ErrorCode404.CHAT_ROOM_NOT_FOUND));
+        if(chatRoom.getStatus().equals(ChatStatus.CLOSED)){
+            throw new ChatException(ErrorCode400.CHAT_ROOM_CLOSED);
+        }
+        return chatRoom;
     }
 
     public void manageChatCollections(String roomId) {
