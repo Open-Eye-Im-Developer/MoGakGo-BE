43e2f54a24de40c732dd83a8dc1559bc22ed96be
JIN-076 <andy230@naver.com>
2024-02-24 00:33:17 +0900

    [FEAT] senderId, receiverId를 이용해 ProfileCardLike 조회 메서드 구현

--- src/main/java/io/oeid/mogakgo/domain/profile/infrastructure/ProfileCardLikeJpaRepository.java
+++ src/main/java/io/oeid/mogakgo/domain/profile/infrastructure/ProfileCardLikeJpaRepository.java
@@ -1,9 +1,13 @@
 package io.oeid.mogakgo.domain.profile.infrastructure;
 
 import io.oeid.mogakgo.domain.profile.domain.entity.ProfileCardLike;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProfileCardLikeJpaRepository extends JpaRepository<ProfileCardLike, Long>,
     ProfileCardLikeRepositoryCustom {
 
+    @Query("select pcl from ProfileCardLike pcl where pcl.sender.id = :senderId and pcl.receiver.id = :receiverId")
+    Optional<ProfileCardLike> findBySenderAndReceiver(Long senderId, Long receiverId);
 }
--- src/main/java/io/oeid/mogakgo/domain/profile/infrastructure/ProfileCardLikeRepositoryCustom.java
+++ src/main/java/io/oeid/mogakgo/domain/profile/infrastructure/ProfileCardLikeRepositoryCustom.java
@@ -2,9 +2,7 @@ package io.oeid.mogakgo.domain.profile.infrastructure;
 
 import io.oeid.mogakgo.common.base.CursorPaginationInfoReq;
 import io.oeid.mogakgo.common.base.CursorPaginationResult;
-import io.oeid.mogakgo.domain.profile.domain.entity.ProfileCardLike;
 import io.oeid.mogakgo.domain.profile.presentation.dto.res.UserProfileLikeInfoAPIRes;
-import java.util.Optional;
 
 public interface ProfileCardLikeRepositoryCustom {
 
@@ -12,5 +10,4 @@ public interface ProfileCardLikeRepositoryCustom {
     Long getLikeCount(Long userId);
     CursorPaginationResult<UserProfileLikeInfoAPIRes> getLikeInfoBySender(
         Long senderId, CursorPaginationInfoReq pageable);
-    Optional<ProfileCardLike> findBySenderAndReceiver(Long senderId, Long receiverId);
 }
