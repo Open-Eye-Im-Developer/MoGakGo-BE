4afa5f2f26d4648005a3753d4c0e3f0452d39200
JIN-076 <andy230@naver.com>
2024-02-27 00:42:12 +0900

    [FEAT] 사용자가 생성한 프로젝트 리스트를 조회하는 메서드를 queryDsl로 구현, 커서 기반 페이지네이션 적용, 삭제되지 않은 프로젝트에 대한 조건 추가

--- src/main/java/io/oeid/mogakgo/domain/project/infrastructure/ProjectRepositoryCustom.java
+++ src/main/java/io/oeid/mogakgo/domain/project/infrastructure/ProjectRepositoryCustom.java
@@ -5,12 +5,17 @@ import io.oeid.mogakgo.common.base.CursorPaginationResult;
 import io.oeid.mogakgo.domain.geo.domain.enums.Region;
 import io.oeid.mogakgo.domain.project.domain.entity.enums.ProjectStatus;
 import io.oeid.mogakgo.domain.project.presentation.dto.res.ProjectDetailAPIRes;
+import io.oeid.mogakgo.domain.project.presentation.dto.res.ProjectInfoAPIRes;
 import java.util.List;
 
 public interface ProjectRepositoryCustom {
 
     CursorPaginationResult<ProjectDetailAPIRes> findByConditionWithPagination(
         Long userId, Region region, ProjectStatus projectStatus, CursorPaginationInfoReq pageable
+    );
+
+    CursorPaginationResult<ProjectInfoAPIRes> findByCreatorIdWithPagination(
+        Long userId, CursorPaginationInfoReq pageable
     );
 
     List<Region> getDensityRankProjectsByRegion(int limit);
