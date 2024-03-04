d476b53a73bf79fea4f8de489adeb99ba435478b
KyungMin Lee <hello.tidavid@gmail.com>
2024-03-04 17:59:31 +0900

    Feat/#151 notification check (#202)
    * [FEAT] Notification 알림 확인 API 구현
    
    * [FEAT] Swagger 구현

---
+++ src/main/java/io/oeid/mogakgo/core/properties/swagger/error/SwaggerNotificationErrorExamples.java
@@ -0,0 +1,9 @@
+package io.oeid.mogakgo.core.properties.swagger.error;
+
+public class SwaggerNotificationErrorExamples {
+
+    public static final String NOTIFICATION_FCM_TOKEN_NOT_FOUND = "{\"timestamp\":\"2024-02-17T10:07:31.404Z\",\"statusCode\":404,\"code\":\"E060301\",\"message\":\"해당 유저의 FCM 토큰이 존재하지 않습니다.\"}";
+    public static final String NOTIFICATION_NOT_FOUND = "{\"timestamp\":\"2024-02-17T10:07:31.404Z\",\"statusCode\":404,\"code\":\"E060302\",\"message\":\"해당 알림이 존재하지 않습니다.\"}";
+    private SwaggerNotificationErrorExamples() {
+    }
+}
--- src/main/java/io/oeid/mogakgo/exception/code/ErrorCode404.java
+++ src/main/java/io/oeid/mogakgo/exception/code/ErrorCode404.java
@@ -9,6 +9,7 @@ public enum ErrorCode404 implements ErrorCode {
     PROJECT_NOT_FOUND("E030301", "해당 프로젝트가 존재하지 않습니다."),
     PROFILE_CARD_NOT_FOUND("E040301", "해당 프로필 카드가 존재하지 않습니다."),
     NOTIFICATION_FCM_TOKEN_NOT_FOUND("E060301", "해당 유저의 FCM 토큰이 존재하지 않습니다."),
+    NOTIFICATION_NOT_FOUND("E060302", "해당 알림이 존재하지 않습니다."),
     PROJECT_JOIN_REQUEST_NOT_FOUND("E050301", "해당 프로젝트 참여 요청이 존재하지 않습니다."),
     MATCHING_NOT_FOUND("E090301", "해당 매칭이 존재하지 않습니다."),
     CHAT_ROOM_NOT_FOUND("E110301", "해당 채팅방이 존재하지 않습니다."),
