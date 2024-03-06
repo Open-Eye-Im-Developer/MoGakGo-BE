7e50369b6e50a7b300e143d545a2ed6abe48532e
JIN-076 <andy230@naver.com>
2024-03-07 02:21:12 +0900

    [REFACT] 프로필 카드 리스트 조회 로직을 QueryDsl로 구현, 조회하려는 프로필 카드에 사용자가 '찔러보기' 요청을 보냈는지의 여부를 확인하는 로직 추가 구현

--- src/main/java/io/oeid/mogakgo/domain/profile/infrastructure/ProfileCardRepositoryCustomImpl.java
+++ src/main/java/io/oeid/mogakgo/domain/profile/infrastructure/ProfileCardRepositoryCustomImpl.java
@@ -1,6 +1,7 @@
 package io.oeid.mogakgo.domain.profile.infrastructure;
 
 import static io.oeid.mogakgo.domain.profile.domain.entity.QProfileCard.profileCard;
+import static io.oeid.mogakgo.domain.profile.domain.entity.QProfileCardLike.profileCardLike;
 import static io.oeid.mogakgo.domain.user.domain.QUser.user;
 
 import com.querydsl.core.types.dsl.BooleanExpression;
@@ -9,10 +10,13 @@ import io.oeid.mogakgo.common.base.CursorPaginationInfoReq;
 import io.oeid.mogakgo.common.base.CursorPaginationResult;
 import io.oeid.mogakgo.domain.geo.domain.enums.Region;
 import io.oeid.mogakgo.domain.profile.domain.entity.ProfileCard;
+import io.oeid.mogakgo.domain.profile.domain.entity.ProfileCardLike;
+import io.oeid.mogakgo.domain.profile.presentation.dto.res.UserProfileInfoAPIRes;
 import io.oeid.mogakgo.domain.user.domain.UserDevelopLanguageTag;
 import io.oeid.mogakgo.domain.user.domain.UserWantedJobTag;
 import io.oeid.mogakgo.domain.user.presentation.dto.res.UserPublicApiResponse;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -23,15 +27,16 @@ public class ProfileCardRepositoryCustomImpl implements ProfileCardRepositoryCus
     private final JPAQueryFactory jpaQueryFactory;
 
     @Override
-    public CursorPaginationResult<UserPublicApiResponse> findByConditionWithPagination(
+    public CursorPaginationResult<UserProfileInfoAPIRes> findByConditionWithPagination(
         Long userId, Region region, CursorPaginationInfoReq pageable
     ) {
+
         List<ProfileCard> entities = jpaQueryFactory.selectFrom(profileCard)
             .innerJoin(profileCard.user, user)
             .on(profileCard.user.id.eq(user.id))
             .where(
                 cursorIdCondition(pageable.getCursorId()),
-                userIdEq(userId),
+                excludeUserId(userId),
                 regionEq(region),
                 deletedProfileCardEq()
             )
@@ -40,20 +45,24 @@ public class ProfileCardRepositoryCustomImpl implements ProfileCardRepositoryCus
             .limit(pageable.getPageSize() + 1)
             .fetch();
 
-        List<UserPublicApiResponse> result = entities.stream().map(
-            profileCard -> new UserPublicApiResponse(
-                profileCard.getUser().getId(),
-                profileCard.getUser().getUsername(),
-                profileCard.getUser().getGithubId(),
-                profileCard.getUser().getAvatarUrl(),
-                profileCard.getUser().getGithubUrl(),
-                profileCard.getUser().getBio(),
-                profileCard.getUser().getJandiRate(),
-                profileCard.getUser().getAchievement() != null ? profileCard.getUser().getAchievement().getTitle() : null,
-                profileCard.getUser().getUserDevelopLanguageTags().stream().map(
-                    UserDevelopLanguageTag::getDevelopLanguage).map(String::valueOf).toList(),
-                profileCard.getUser().getUserWantedJobTags().stream().map(
-                    UserWantedJobTag::getWantedJob).map(String::valueOf).toList()
+        List<UserProfileInfoAPIRes> result = entities.stream().map(
+            profileCard -> new UserProfileInfoAPIRes(
+                new UserPublicApiResponse(
+                    profileCard.getUser().getId(),
+                    profileCard.getUser().getUsername(),
+                    profileCard.getUser().getGithubId(),
+                    profileCard.getUser().getAvatarUrl(),
+                    profileCard.getUser().getGithubUrl(),
+                    profileCard.getUser().getBio(),
+                    profileCard.getUser().getJandiRate(),
+                    profileCard.getUser().getAchievement() != null ? profileCard.getUser().getAchievement().getTitle() : null,
+                    profileCard.getUser().getUserDevelopLanguageTags().stream().map(
+                        UserDevelopLanguageTag::getDevelopLanguage).map(String::valueOf).toList(),
+                    profileCard.getUser().getUserWantedJobTags().stream().map(
+                        UserWantedJobTag::getWantedJob).map(String::valueOf).toList()
+                ),
+                findProfileCardLikeBySenderId(profileCard.getUser().getId(), userId)
+                    .isPresent() ? Boolean.TRUE : Boolean.FALSE
             )
         ).toList();
 
@@ -62,16 +71,26 @@ public class ProfileCardRepositoryCustomImpl implements ProfileCardRepositoryCus
         );
     }
 
+    private Optional<ProfileCardLike> findProfileCardLikeBySenderId(Long receiverId, Long userId) {
+        return Optional.ofNullable(jpaQueryFactory.selectFrom(profileCardLike)
+            .join(profileCardLike.receiver)
+            .where(
+                profileCardLike.receiver.id.eq(receiverId),
+                profileCardLike.sender.id.eq(userId)
+            )
+            .fetchOne());
+    }
+
     private BooleanExpression regionEq(Region region) {
         return region != null ? profileCard.user.region.eq(region) : null;
     }
 
-    private BooleanExpression userIdEq(Long userId) {
-        return userId != null ? profileCard.user.id.eq(userId) : null;
-    }
-
     private BooleanExpression cursorIdCondition(Long cursorId) {
         return cursorId != null ? profileCard.id.lt(cursorId) : null;
+    }
+
+    private BooleanExpression excludeUserId(Long userId) {
+        return profileCard.user.id.ne(userId);
     }
 
     private BooleanExpression deletedProfileCardEq() {
