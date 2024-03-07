c4e7a5229b7e3e22c57496c1668c2902c1385002
KyungMin Lee <hello.tidavid@gmail.com>
2024-03-07 13:56:49 +0900

    [FEAT] Redirect Url 추가 전송하는 Push Notification 구현

--- src/main/java/io/oeid/mogakgo/domain/notification/application/FCMNotificationService.java
+++ src/main/java/io/oeid/mogakgo/domain/notification/application/FCMNotificationService.java
@@ -4,24 +4,35 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
+import com.google.firebase.messaging.WebpushConfig;
+import com.google.firebase.messaging.WebpushFcmOptions;
 import io.oeid.mogakgo.domain.notification.domain.vo.FCMToken;
 import io.oeid.mogakgo.domain.notification.exception.NotificationException;
 import io.oeid.mogakgo.domain.notification.infrastructure.FCMTokenJpaRepository;
 import io.oeid.mogakgo.domain.user.application.UserCommonService;
 import io.oeid.mogakgo.exception.code.ErrorCode404;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 public class FCMNotificationService {
 
     private final FCMTokenJpaRepository fcmTokenRepository;
     private final UserCommonService userCommonService;
     private final FirebaseMessaging firebaseMessaging;
+    private final String clientUrl;
+
+    public FCMNotificationService(FCMTokenJpaRepository fcmTokenRepository,
+        UserCommonService userCommonService, FirebaseMessaging firebaseMessaging,
+        @Value("${auth.client-url}") String clientUrl) {
+        this.fcmTokenRepository = fcmTokenRepository;
+        this.userCommonService = userCommonService;
+        this.firebaseMessaging = firebaseMessaging;
+        this.clientUrl = clientUrl;
+    }
 
     @Transactional
     public void manageToken(Long userId, String fcmToken) {
@@ -52,6 +63,33 @@ public class FCMNotificationService {
         }
         log.info("sendNotification End");
     }
+
+    public void sendNotification(Long userId, String title, String body, String redirectUri) {
+        log.info("sendNotification Start");
+        String fcmToken = getFCMToken(userId);
+        WebpushConfig webpushConfig = WebpushConfig.builder()
+            .setFcmOptions(WebpushFcmOptions.builder()
+                .setLink(clientUrl + redirectUri)
+                .build())
+            .build();
+        // send notification
+        Message message = Message.builder()
+            .setNotification(Notification.builder()
+                .setTitle(title)
+                .setBody(body)
+                .build())
+            .setToken(fcmToken)
+            .setWebpushConfig(webpushConfig)
+            .build();
+        try {
+            String response = firebaseMessaging.send(message);
+            log.info("Successfully sent message: " + response);
+        } catch (FirebaseMessagingException e) {
+            log.error("Error sending message: " + e.getMessage());
+        }
+        log.info("sendNotification End");
+    }
+
 
     private String getFCMToken(Long userId) {
         return fcmTokenRepository.findById(userId)
