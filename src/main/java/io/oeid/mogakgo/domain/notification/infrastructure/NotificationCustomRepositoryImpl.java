8fc53d49443b0c17ce2d99c5c68dabeedfb98e8b
KyungMin Lee <hello.tidavid@gmail.com>
2024-03-08 01:07:23 +0900

    [REFACT] `Notification` 서비스 리펙토링

--- src/main/java/io/oeid/mogakgo/domain/notification/infrastructure/NotificationCustomRepositoryImpl.java
+++ src/main/java/io/oeid/mogakgo/domain/notification/infrastructure/NotificationCustomRepositoryImpl.java
@@ -2,16 +2,10 @@ package io.oeid.mogakgo.domain.notification.infrastructure;
 
 
 import static io.oeid.mogakgo.domain.notification.domain.QNotification.notification;
-import static io.oeid.mogakgo.domain.user.domain.QUser.user;
 
-import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import io.oeid.mogakgo.common.base.CursorPaginationInfoReq;
-import io.oeid.mogakgo.common.base.CursorPaginationResult;
-import io.oeid.mogakgo.domain.notification.presentation.dto.res.NotificationPublicApiRes;
-import io.oeid.mogakgo.domain.notification.presentation.vo.NotificationDataVo;
-import io.oeid.mogakgo.domain.notification.presentation.vo.NotificationSenderVo;
+import io.oeid.mogakgo.domain.notification.domain.Notification;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -22,36 +16,10 @@ public class NotificationCustomRepositoryImpl implements NotificationCustomRepos
 
     private final JPAQueryFactory jpaQueryFactory;
 
-    public CursorPaginationResult<NotificationPublicApiRes> findByUserIdWithPagination(Long userId,
-        CursorPaginationInfoReq pageable) {
-        List<NotificationPublicApiRes> result = jpaQueryFactory.select(
-                Projections.constructor(
-                    NotificationPublicApiRes.class,
-                    notification.id,
-                    notification.notificationTag,
-                    Projections.constructor(
-                        NotificationSenderVo.class,
-                        notification.sender.username,
-                        notification.sender.id,
-                        notification.sender.avatarUrl
-                    ),
-                    Projections.constructor(
-                        NotificationDataVo.class,
-                        notification.detailData,
-                        notification.createdAt
-                    )
-                )
-            )
-            .from(notification)
-            .join(notification.sender, user)
-            .where(
-                cursorIdCondition(pageable.getCursorId()),
-                notification.receiver.id.eq(userId))
-            .orderBy(notification.id.desc())
-            .limit(pageable.getPageSize() + 1L)
-            .fetch();
-        return CursorPaginationResult.fromDataWithExtraItemForNextCheck(result,
-            pageable.getPageSize());
+    public List<Notification> findByUserIdWithPagination(Long userId, Long cursorId, int pageSize) {
+        return jpaQueryFactory.selectFrom(notification)
+            .where(cursorIdCondition(cursorId), notification.user.id.eq(userId))
+            .orderBy(notification.id.desc()).limit(pageSize + 1L).fetch();
     }
 
     private BooleanExpression cursorIdCondition(Long cursorId) {
