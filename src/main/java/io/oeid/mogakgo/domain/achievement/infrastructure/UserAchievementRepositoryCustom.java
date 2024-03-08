90e59cacac281cc1896368851f3190f4ecd5ff51
JIN-076 <57834671+JIN-076@users.noreply.github.com>
2024-03-08 20:00:03 +0900

    [REFACT] 사용자의 업적 상세 조회 API를 하나로 합침, 업적 상세 조회 로직 구현 (#239)
    * [REFACT] 불필요한 DTO 클래스 제거
    
    * [REFACT] 사용자의 업적 조회 API를 하나로 합치도록 리팩토링, 불필요한 Controller와 메서드 제거, Swagger 수정
    
    * [REFACT] 불필요한 메서드 제거, 사용자의 업적을 조회하는 메서드를 하나로 합치도록 사용자의 업적 상세 조회 로직 구현
    
    * [REFACT] Swagger 설명 수정

--- src/main/java/io/oeid/mogakgo/domain/achievement/infrastructure/UserAchievementRepositoryCustom.java
+++ src/main/java/io/oeid/mogakgo/domain/achievement/infrastructure/UserAchievementRepositoryCustom.java
@@ -1,11 +1,9 @@
 package io.oeid.mogakgo.domain.achievement.infrastructure;
 
-import io.oeid.mogakgo.domain.achievement.application.dto.res.AchievementInfoRes;
 import io.oeid.mogakgo.domain.achievement.application.dto.res.UserAchievementInfoRes;
 import java.util.List;
 
 public interface UserAchievementRepositoryCustom {
 
-    List<UserAchievementInfoRes> getAchievedOrInProcessUserAchievementInfo(Long userId);
-    List<AchievementInfoRes> getNonAchievedAchievementInfo(Long userId);
+    List<UserAchievementInfoRes> getAchievementInfoAboutUser(Long userId);
 }
--- src/main/java/io/oeid/mogakgo/domain/achievement/infrastructure/UserAchievementRepositoryCustomImpl.java
+++ src/main/java/io/oeid/mogakgo/domain/achievement/infrastructure/UserAchievementRepositoryCustomImpl.java
@@ -1,21 +1,19 @@
 package io.oeid.mogakgo.domain.achievement.infrastructure;
 
-import static io.oeid.mogakgo.domain.user.domain.QUser.user;
 import static io.oeid.mogakgo.domain.achievement.domain.entity.QUserAchievement.userAchievement;
 import static io.oeid.mogakgo.domain.achievement.domain.entity.QUserActivity.userActivity;
 import static io.oeid.mogakgo.domain.achievement.domain.entity.QAchievement.achievement;
 
 import com.querydsl.core.Tuple;
-import com.querydsl.core.types.Projections;
-import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.Expressions;
-import com.querydsl.core.types.dsl.NumberPath;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import io.oeid.mogakgo.domain.achievement.application.dto.res.AchievementInfoRes;
 import io.oeid.mogakgo.domain.achievement.application.dto.res.UserAchievementInfoRes;
 import io.oeid.mogakgo.domain.achievement.domain.entity.QAchievement;
 import io.oeid.mogakgo.domain.achievement.domain.entity.QUserAchievement;
+import io.oeid.mogakgo.domain.achievement.domain.entity.QUserActivity;
+import io.oeid.mogakgo.domain.achievement.domain.entity.enums.RequirementType;
+import java.util.Comparator;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -28,105 +26,87 @@ public class UserAchievementRepositoryCustomImpl implements UserAchievementRepos
 
     QAchievement achievement1 = new QAchievement("achievement1");
     QAchievement achievement2 = new QAchievement("achievement2");
+    QAchievement achievement3 = new QAchievement("achievement3");
+
     QUserAchievement userAchievement1 = new QUserAchievement("userAchievement1");
+    QUserAchievement userAchievement2 = new QUserAchievement("userAchievement2");
+
+    QUserActivity userActivity1 = new QUserActivity("userActivity1");
 
     @Override
-    public List<UserAchievementInfoRes> getAchievedOrInProcessUserAchievementInfo(Long userId) {
+    public List<UserAchievementInfoRes> getAchievementInfoAboutUser(Long userId) {
 
-        List<Tuple> result = jpaQueryFactory.select(userActivity.createdAt.count(), userActivity.activityType.stringValue())
-            .from(userActivity)
-            .innerJoin(userActivity.user, user)
-            .where(userActivity.activityType.eq(achievement.activityType))
-            .fetch();
-
-        Integer progressCount = Integer.valueOf(String.valueOf(result.get(0).get(0, Long.class)));
-        String activityType = result.get(0).get(1, String.class);
-
-        Long achievementId = jpaQueryFactory.select(achievement.id)
-            .from(achievement)
-            .where(
-                achievement.activityType.stringValue().eq(activityType),
-                achievement.requirementValue.gt(progressCount)
+        List<Tuple> sql1 = jpaQueryFactory.select(
+            Expressions.numberPath(Long.class, String.valueOf(userId)),
+            achievement.id,
+            achievement.title,
+            achievement.imgUrl,
+            achievement.description,
+            achievement.requirementType,
+            achievement.requirementValue,
+            Expressions.numberPath(Long.class, String.valueOf(0)),
+            Expressions.booleanPath(String.valueOf(false))
+        )
+        .from(achievement)
+        .leftJoin(userAchievement)
+            .on(userAchievement.achievement.id.eq(achievement.id),
+                userAchievement.user.id.eq(userId))
+        .where(
+            userAchievement.user.id.isNull(),
+            achievement.id.in(
+                JPAExpressions.select(achievement1.id.min())
+                    .from(achievement1)
+                    .groupBy(achievement1.activityType)
             )
-            .orderBy(achievement.requirementValue.asc())
-            .limit(1)
-            .fetchOne();
+        )
+        .fetch();
 
-        return jpaQueryFactory.select(
-                Projections.constructor(
-                    UserAchievementInfoRes.class,
-                    userAchievement.user.id,
-                    userAchievement.achievement.id,
-                    userAchievement.achievement.title,
-                    userAchievement.achievement.imgUrl,
-                    userAchievement.achievement.description,
-                    userAchievement.achievement.requirementType,
-                    userAchievement.achievement.requirementValue,
-                    Expressions.numberPath(Integer.class, String.valueOf(progressCount)),
-                    userAchievement.completed
-                )
+        List<Tuple> sql2 = jpaQueryFactory.select(
+            userAchievement1.user.id,
+            achievement2.id,
+            achievement2.title,
+            achievement2.imgUrl,
+            achievement2.description,
+            achievement2.requirementType,
+            achievement2.requirementValue,
+            Expressions.numberPath(Long.class, String.valueOf(userActivity.createdAt.count())),
+            userAchievement1.completed
+        )
+        .from(achievement2)
+        .innerJoin(userAchievement1).on(
+            userAchievement1.achievement.id.eq(achievement2.id),
+            userAchievement1.user.id.eq(userId)
+        )
+        .innerJoin(userActivity).on(userActivity.activityType.eq(achievement2.activityType))
+        .where(
+            achievement2.id.in(
+                JPAExpressions.select(achievement3.id.max())
+                    .from(userActivity1)
+                    .innerJoin(achievement3)
+                        .on(achievement3.activityType.eq(userActivity1.activityType),
+                            userActivity.user.id.eq(userId))
+                    .innerJoin(userAchievement2).
+                        on(userAchievement2.achievement.id.eq(achievement3.id))
+                    .groupBy(userActivity1.activityType)
             )
-            .from(userAchievement)
-            .innerJoin(userAchievement.user, user).on(userAchievement.user.id.eq(user.id))
-            .innerJoin(userAchievement.achievement, achievement).on(userAchievement.achievement.id.eq(achievement.id))
-            .where(
-                userIdEq(userId),
-                achievementIdEq(achievementId)
+        )
+        .fetch();
+
+        sql1.addAll(sql2);
+
+        return sql1.stream()
+            .sorted(Comparator.comparing(tuple -> tuple.get(1, Long.class))).map(
+            tuple -> new UserAchievementInfoRes(
+                userId,
+                tuple.get(1, Long.class),
+                tuple.get(2, String.class),
+                tuple.get(3, String.class),
+                tuple.get(4, String.class),
+                tuple.get(5, RequirementType.class),
+                tuple.get(6, Integer.class),
+                Integer.valueOf(String.valueOf(tuple.get(6, Long.class))),
+                tuple.get(8, Boolean.class)
             )
-            .groupBy(userAchievement.user.id, userAchievement.achievement.id)
-            .fetch();
-    }
-
-    @Override
-    public List<AchievementInfoRes> getNonAchievedAchievementInfo(Long userId) {
-
-        NumberPath<Long> id = Expressions.numberPath(Long.class, String.valueOf(userId));
-
-        return jpaQueryFactory.select(
-                Projections.constructor(
-                    AchievementInfoRes.class,
-                    id.as("id"),
-                    achievement.id,
-                    achievement.title,
-                    achievement.imgUrl,
-                    achievement.description,
-                    achievement.requirementType,
-                    achievement.requirementValue
-                )
-            )
-            .from(achievement)
-            .where(
-                JPAExpressions.selectOne()
-                    .from(userAchievement)
-                    .where(
-                        userAchievement.achievement.id.eq(achievement.id),
-                        userAchievement.user.id.eq(id)
-                    )
-                    .isNull(),
-                achievement.id.in(
-                    JPAExpressions.select(achievement1.id.min())
-                        .from(achievement1)
-                        .where(
-                            JPAExpressions.selectOne()
-                                .from(userAchievement1)
-                                .join(achievement2).on(userAchievement1.achievement.id.eq(achievement2.id))
-                                .where(
-                                    userAchievement1.user.id.eq(id),
-                                    achievement2.activityType.stringValue().eq(achievement1.activityType.stringValue())
-                                )
-                                .isNull()
-                        )
-                        .groupBy(achievement1.activityType)
-                )
-            )
-            .fetch();
-    }
-
-    private BooleanExpression userIdEq(Long userId) {
-        return userId != null ? userAchievement.user.id.eq(userId) : null;
-    }
-
-    private BooleanExpression achievementIdEq(Long achievementId) {
-        return achievementId != null ? userAchievement.achievement.id.eq(achievementId) : null;
+        ).toList();
     }
 }
