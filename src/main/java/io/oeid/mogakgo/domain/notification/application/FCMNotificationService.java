b9fbf35d7ef0b5eead47df0996a88ec4ed9897c6
KyungMin Lee <hello.tidavid@gmail.com>
2024-03-08 12:41:13 +0900

    [REFACT] fcmToken이 존재하지 않을시 예외 처리 대신 알림을 발송하지 않도록 설정

--- src/main/java/io/oeid/mogakgo/domain/notification/application/FCMNotificationService.java
+++ src/main/java/io/oeid/mogakgo/domain/notification/application/FCMNotificationService.java
@@ -7,10 +7,9 @@ import com.google.firebase.messaging.Notification;
 import com.google.firebase.messaging.WebpushConfig;
 import com.google.firebase.messaging.WebpushFcmOptions;
 import io.oeid.mogakgo.domain.notification.domain.vo.FCMToken;
-import io.oeid.mogakgo.domain.notification.exception.NotificationException;
 import io.oeid.mogakgo.domain.notification.infrastructure.FCMTokenJpaRepository;
 import io.oeid.mogakgo.domain.user.application.UserCommonService;
-import io.oeid.mogakgo.exception.code.ErrorCode404;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -46,55 +45,59 @@ public class FCMNotificationService {
 
     public void sendNotification(Long userId, String title, String body) {
         log.info("sendNotification Start");
-        String fcmToken = getFCMToken(userId);
+        getFCMToken(userId).ifPresent(
+            fcmToken -> {
+                Message message = Message.builder()
+                    .setNotification(Notification.builder()
+                        .setTitle(title)
+                        .setBody(body)
+                        .build())
+                    .setToken(fcmToken)
+                    .build();
+                try {
+                    String response = firebaseMessaging.send(message);
+                    log.info("Successfully sent message: " + response);
+                } catch (FirebaseMessagingException e) {
+                    log.error("Error sending message: " + e.getMessage());
+                }
+            }
+        );
         // send notification
-        Message message = Message.builder()
-            .setNotification(Notification.builder()
-                .setTitle(title)
-                .setBody(body)
-                .build())
-            .setToken(fcmToken)
-            .build();
-        try {
-            String response = firebaseMessaging.send(message);
-            log.info("Successfully sent message: " + response);
-        } catch (FirebaseMessagingException e) {
-            log.error("Error sending message: " + e.getMessage());
-        }
         log.info("sendNotification End");
     }
 
     public void sendNotification(Long userId, String title, String body, String redirectUri) {
         log.info("sendNotification Start");
-        String fcmToken = getFCMToken(userId);
-        WebpushConfig webpushConfig = WebpushConfig.builder()
-            .setFcmOptions(WebpushFcmOptions.builder()
-                .setLink(clientUrl + redirectUri)
-                .build())
-            .build();
-        // send notification
-        Message message = Message.builder()
-            .setNotification(Notification.builder()
-                .setTitle(title)
-                .setBody(body)
-                .build())
-            .setToken(fcmToken)
-            .setWebpushConfig(webpushConfig)
-            .build();
-        try {
-            String response = firebaseMessaging.send(message);
-            log.info("Successfully sent message: " + response);
-        } catch (FirebaseMessagingException e) {
-            log.error("Error sending message: " + e.getMessage());
-        }
+        getFCMToken(userId).ifPresent(
+            fcmToken -> {
+                WebpushConfig webpushConfig = WebpushConfig.builder()
+                    .setFcmOptions(WebpushFcmOptions.builder()
+                        .setLink(clientUrl + redirectUri)
+                        .build())
+                    .build();
+                // send notification
+                Message message = Message.builder()
+                    .setNotification(Notification.builder()
+                        .setTitle(title)
+                        .setBody(body)
+                        .build())
+                    .setToken(fcmToken)
+                    .setWebpushConfig(webpushConfig)
+                    .build();
+                try {
+                    String response = firebaseMessaging.send(message);
+                    log.info("Successfully sent message: " + response);
+                } catch (FirebaseMessagingException e) {
+                    log.error("Error sending message: " + e.getMessage());
+                }
+            }
+        );
         log.info("sendNotification End");
     }
 
 
-    private String getFCMToken(Long userId) {
+    private Optional<String> getFCMToken(Long userId) {
         return fcmTokenRepository.findById(userId)
-            .map(FCMToken::getToken)
-            .orElseThrow(
-                () -> new NotificationException(ErrorCode404.NOTIFICATION_FCM_TOKEN_NOT_FOUND));
+            .map(FCMToken::getToken);
     }
 }
