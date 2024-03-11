ce4c09dd3a439850f20c8eceef88ee3900eae227
KyungMin Lee <hello.tidavid@gmail.com>
2024-03-11 21:11:02 +0900

    Feat/#150 matching success notification (#250)
    * [FEAT] `NotificationMessage` Enum 추가
    
    * [REFACT] `NotificationMessage` Enum 사용에 따른 리펙토링
    
    * [FEAT] `FCMNotificationType` Enum 추가
    
    * [REFACT] `FCMNotificationType` Enum 추가에 따른 리펙토링
    
    * [REFACT] `NotificationMessage` title 필드 추가
    
    * [FEAT] 매칭 요청 수락시 FCM 알림 발송 기능 추가
    
    * [REMOVE] 사용하지 않는 Enum 필드 제거
    
    * [FEAT] 매칭 성공시 PULL 알림 생성 추가

--- src/main/java/io/oeid/mogakgo/domain/project_join_req/application/ProjectJoinRequestService.java
+++ src/main/java/io/oeid/mogakgo/domain/project_join_req/application/ProjectJoinRequestService.java
@@ -1,5 +1,7 @@
 package io.oeid.mogakgo.domain.project_join_req.application;
 
+import static io.oeid.mogakgo.domain.notification.domain.enums.FCMNotificationType.MATCHING_SUCCEEDED;
+import static io.oeid.mogakgo.domain.notification.domain.enums.NotificationMessage.MATCHING_SUCCESS_MESSAGE;
 import static io.oeid.mogakgo.exception.code.ErrorCode400.INVALID_MATCHING_USER_TO_ACCEPT;
 import static io.oeid.mogakgo.exception.code.ErrorCode400.INVALID_SENDER_TO_ACCEPT;
 import static io.oeid.mogakgo.exception.code.ErrorCode400.PROJECT_JOIN_REQUEST_SHOULD_BE_ONLY_ONE;
@@ -12,6 +14,8 @@ import io.oeid.mogakgo.common.base.CursorPaginationInfoReq;
 import io.oeid.mogakgo.common.base.CursorPaginationResult;
 import io.oeid.mogakgo.domain.matching.application.MatchingService;
 import io.oeid.mogakgo.domain.matching.application.UserMatchingService;
+import io.oeid.mogakgo.domain.notification.application.FCMNotificationService;
+import io.oeid.mogakgo.domain.notification.application.NotificationService;
 import io.oeid.mogakgo.domain.project.domain.entity.Project;
 import io.oeid.mogakgo.domain.project.exception.ProjectException;
 import io.oeid.mogakgo.domain.project.infrastructure.ProjectJpaRepository;
@@ -38,8 +42,9 @@ public class ProjectJoinRequestService {
     private final ProjectJpaRepository projectRepository;
     private final UserMatchingService userMatchingService;
     private final MatchingService matchingService;
-
+    private final FCMNotificationService fcmNotificationService;
     private final UserCommonService userCommonService;
+    private final NotificationService notificationService;
 
     @Transactional
     public Long create(Long userId, ProjectJoinCreateReq request) {
@@ -95,7 +100,11 @@ public class ProjectJoinRequestService {
         } catch (ProjectJoinRequestException e) {
             // TODO: 로그 처리
         }
-
+        fcmNotificationService.sendNotification(projectJoinRequest.getSender().getId(),
+            MATCHING_SUCCESS_MESSAGE.getTitle(), MATCHING_SUCCESS_MESSAGE.getMessage(),
+            MATCHING_SUCCEEDED);
+        notificationService.createMatchingSuccessNotification(projectJoinRequest.getSender().getId(),
+            projectJoinRequest.getProject());
         return matchingId;
     }
 
