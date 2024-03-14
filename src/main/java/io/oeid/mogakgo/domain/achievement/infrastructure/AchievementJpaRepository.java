f54a8c5606858709fcd905ab1dc5776a54b087f6
JIN-076 <andy230@naver.com>
2024-03-14 22:38:29 +0900

    [FEAT] 사용자가 현재 진행중인 연속성 업적 타입 리스트 조회 구현

--- src/main/java/io/oeid/mogakgo/domain/achievement/infrastructure/AchievementJpaRepository.java
+++ src/main/java/io/oeid/mogakgo/domain/achievement/infrastructure/AchievementJpaRepository.java
@@ -1,10 +1,25 @@
 package io.oeid.mogakgo.domain.achievement.infrastructure;
 
 import io.oeid.mogakgo.domain.achievement.domain.entity.Achievement;
+import io.oeid.mogakgo.domain.achievement.domain.entity.enums.ActivityType;
+import io.oeid.mogakgo.domain.achievement.domain.entity.enums.RequirementType;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AchievementJpaRepository extends JpaRepository<Achievement, Long> {
+
+    @Query("select ac from Achievement ac where ac.id = :id")
+    Optional<Achievement> findById(Long id);
+
+    @Query("""
+        select ac.activityType from Achievement ac join UserAchievement ua
+        on ac.id = ua.achievement.id and ua.user.id = :userId
+        where ac.requirementType = :requirementType group by ac.activityType
+    """)
+    List<ActivityType> findActivityTypeInProgress(Long userId, RequirementType requirementType);
 
 }
