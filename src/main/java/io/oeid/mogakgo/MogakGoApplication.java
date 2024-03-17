9d0b1b2dde94e8775eee2cc5d30096c3cc0e2b3a
JIN-076 <andy230@naver.com>
2024-03-17 23:59:13 +0900

    [FEAT] 비동기 처리를 위한 비동기 활성화 어노테이션 추가

--- src/main/java/io/oeid/mogakgo/MogakGoApplication.java
+++ src/main/java/io/oeid/mogakgo/MogakGoApplication.java
@@ -3,9 +3,11 @@ package io.oeid.mogakgo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableAsync
 public class MogakGoApplication {
 
     public static void main(String[] args) {
