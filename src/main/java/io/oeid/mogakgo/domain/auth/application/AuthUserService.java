163be7156e6da3b00bfb82bc186c7f666f2c9aee
KyungMin Lee <hello.tidavid@gmail.com>
2024-03-18 18:12:23 +0900

    Refact/#269 oauth2 login (#271)
    * [REFACT] Spring OAuth2 Client 의존 제거
    
    * [REFACT] OAuth2 로그인 방식 변경

--- src/main/java/io/oeid/mogakgo/domain/auth/application/AuthUserService.java
+++ src/main/java/io/oeid/mogakgo/domain/auth/application/AuthUserService.java
@@ -1,8 +1,9 @@
 package io.oeid.mogakgo.domain.auth.application;
 
-import io.oeid.mogakgo.domain.user.application.dto.res.UserOAuth2Response;
+import io.oeid.mogakgo.domain.auth.application.dto.res.AuthOAuth2Response;
 import io.oeid.mogakgo.domain.user.domain.User;
 import io.oeid.mogakgo.domain.user.infrastructure.UserJpaRepository;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,14 +15,16 @@ public class AuthUserService {
     private final UserJpaRepository userRepository;
 
     @Transactional
-    public UserOAuth2Response manageOAuth2User(long githubPk, String githubId, String avatarUrl,
-        String githubUrl,
-        String repositoryUrl) {
-        User user = userRepository.findByGithubPk(githubPk).orElseGet(() -> userRepository.save(
-            User.of(githubPk, githubId, avatarUrl, githubUrl, repositoryUrl)));
+    public AuthOAuth2Response manageOAuth2User(Map<String, Object> githubUserInfo) {
+        var githubPk = Long.parseLong(githubUserInfo.get("id").toString());
+        var githubId = githubUserInfo.get("login").toString();
+        var avatarUrl = githubUserInfo.get("avatar_url").toString();
+        var githubUrl = githubUserInfo.get("html_url").toString();
+        var repositoryUrl = githubUserInfo.get("repos_url").toString();
+        var user = userRepository.findByGithubPk(githubPk).orElseGet(() ->
+            userRepository.save(User.of(githubPk, githubId, avatarUrl, githubUrl, repositoryUrl)));
         user.updateGithubInfo(githubId, avatarUrl, githubUrl, repositoryUrl);
-        return UserOAuth2Response.from(user);
+        return AuthOAuth2Response.from(user);
     }
-
 
 }
