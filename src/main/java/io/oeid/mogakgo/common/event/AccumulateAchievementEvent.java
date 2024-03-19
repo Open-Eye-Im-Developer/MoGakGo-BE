617eb1aedc272d5927530ce1541d95a3f6392ebd
JIN-076 <andy230@naver.com>
2024-03-19 14:29:17 +0900

    [FEAT] 업적 관련 정보 전달을 위한 필드 추가

--- src/main/java/io/oeid/mogakgo/common/event/AccumulateAchievementEvent.java
+++ src/main/java/io/oeid/mogakgo/common/event/AccumulateAchievementEvent.java
@@ -6,11 +6,14 @@ import lombok.Getter;
 @Getter
 public class AccumulateAchievementEvent extends AchievementEvent {
 
+    private final Integer progressCount;
     private final Boolean completed;
 
     @Builder
-    public AccumulateAchievementEvent(Long userId, Long achievementId, Boolean completed) {
+    public AccumulateAchievementEvent(Long userId, Long achievementId,
+        Integer progressCount, Boolean completed) {
         super(userId, achievementId);
+        this.progressCount = progressCount;
         this.completed = completed;
     }
 }
--- src/main/java/io/oeid/mogakgo/common/event/UserActivityEvent.java
+++ src/main/java/io/oeid/mogakgo/common/event/UserActivityEvent.java
@@ -8,12 +8,17 @@ import lombok.Getter;
 public class UserActivityEvent {
 
     private final Long userId;
+    private final Long achievementId;
     private final ActivityType activityType;
+    private final Integer progressCount;
 
     @Builder
-    private UserActivityEvent(Long userId, ActivityType activityType) {
+    private UserActivityEvent(Long userId, Long achievementId, ActivityType activityType,
+        Integer progressCount) {
         this.userId = userId;
+        this.achievementId = achievementId;
         this.activityType = activityType;
+        this.progressCount = progressCount;
     }
 
 }
