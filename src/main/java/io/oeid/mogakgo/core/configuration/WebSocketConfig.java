1ac23562f6da7e99448ff6fa0ce20d87504486f0
JIN-076 <57834671+JIN-076@users.noreply.github.com>
2024-03-20 13:15:44 +0900

    [BUG] 웹 소켓 활성화를 위한 @EnableWebSocket 추가 (#286)

--- src/main/java/io/oeid/mogakgo/core/configuration/WebSocketConfig.java
+++ src/main/java/io/oeid/mogakgo/core/configuration/WebSocketConfig.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -14,9 +15,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @RequiredArgsConstructor
+@EnableWebSocket
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer,
-    WebSocketConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer, WebSocketConfigurer {
 
     private final ChatInterceptor chatInterceptor;
     private final AchievementSocketHandler achievementSocketHandler;
