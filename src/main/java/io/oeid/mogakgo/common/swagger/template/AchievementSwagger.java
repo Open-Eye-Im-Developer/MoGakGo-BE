2ebfa25de6378c6d7cf7ff6cacaf542eb4967580
JIN-076 <57834671+JIN-076@users.noreply.github.com>
2024-03-21 12:41:33 +0900

    [FEAT] 특정 업적 상세 정보 조회 API 개발 (#314)

--- src/main/java/io/oeid/mogakgo/common/swagger/template/AchievementSwagger.java
+++ src/main/java/io/oeid/mogakgo/common/swagger/template/AchievementSwagger.java
@@ -1,7 +1,9 @@
 package io.oeid.mogakgo.common.swagger.template;
 
+import io.oeid.mogakgo.core.properties.swagger.error.SwaggerAchievementErrorExamples;
 import io.oeid.mogakgo.core.properties.swagger.error.SwaggerUserAchievementErrorExamples;
 import io.oeid.mogakgo.core.properties.swagger.error.SwaggerUserErrorExamples;
+import io.oeid.mogakgo.domain.achievement.presentation.dto.res.AchievementInfoAPIRes;
 import io.oeid.mogakgo.domain.achievement.presentation.dto.res.UserAchievementDetailAPIRes;
 import io.oeid.mogakgo.exception.dto.ErrorResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -37,5 +39,19 @@ public interface AchievementSwagger {
     ResponseEntity<List<UserAchievementDetailAPIRes>> getUserAchievementDetail(
         @Parameter(hidden = true) Long userId,
         @Parameter(description = "업적을 조회하는 사용자 ID", required = true) Long id
+    );
+
+    @Operation(summary = "업적 상세 조회", description = "특정 업적에 대한 상세 정보를 조회할 때 사용하는 API")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "업적 상세 조회 성공"),
+        @ApiResponse(responseCode = "404", description = "요청한 데이터가 존재하지 않음",
+            content = @Content(
+                mediaType = "application/json",
+                schema = @Schema(implementation = ErrorResponse.class),
+                examples = @ExampleObject(name = "E130301", value = SwaggerAchievementErrorExamples.ACHIEVEMENT_NOT_FOUND)
+            ))
+    })
+    ResponseEntity<AchievementInfoAPIRes> getAchievementById(
+        @Parameter(description = "조회하려는 업적 ID", required = true) Long achievementId
     );
 }
