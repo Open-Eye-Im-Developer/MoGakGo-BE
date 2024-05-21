f18b6f4f3c77ebe293b325c6c63bd648d8d13c73
KyungMin Lee <hello.tidavid@gmail.com>
2024-05-21 14:48:12 +0900

    [REFACT] `@WithMockCustomUser` 동작방식 변경 (#372)

--- src/test/java/io/oeid/mogakgo/core/support/WithMockCustomUserSecurityContextFactory.java
+++ src/test/java/io/oeid/mogakgo/core/support/WithMockCustomUserSecurityContextFactory.java
@@ -1,8 +1,8 @@
 package io.oeid.mogakgo.core.support;
 
-import io.oeid.mogakgo.core.properties.JwtProperties;
+import static io.oeid.mogakgo.domain.user.domain.enums.Role.ROLE_USER;
+
 import io.oeid.mogakgo.domain.auth.jwt.JwtAuthenticationToken;
-import io.oeid.mogakgo.domain.auth.jwt.JwtHelper;
 import java.util.List;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -13,31 +13,11 @@ import org.springframework.security.test.context.support.WithSecurityContextFact
 public class WithMockCustomUserSecurityContextFactory implements
     WithSecurityContextFactory<WithMockCustomUser> {
 
-    private static JwtProperties initProperties() {
-        JwtProperties properties = new JwtProperties();
-        properties.setHeader("test");
-        properties.setIssuer("test");
-        properties.setClientSecret("test");
-        properties.setAccessTokenExpiryHour(1);
-        properties.setRefreshTokenExpiryHour(3);
-        return properties;
-    }
-
-    private final JwtHelper jwtHelper;
-
-    public WithMockCustomUserSecurityContextFactory() {
-        this.jwtHelper = new JwtHelper(
-            initProperties()
-        );
-    }
-
     @Override
     public SecurityContext createSecurityContext(WithMockCustomUser mockCustomUser) {
         SecurityContext context = SecurityContextHolder.createEmptyContext();
-        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("ROLE_USER"));
-        var token = jwtHelper.sign(mockCustomUser.userId(),
-            authorities.stream().map(GrantedAuthority::getAuthority).toArray(String[]::new));
-        var auth = new JwtAuthenticationToken(token, null, authorities);
+        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(ROLE_USER.name()));
+        var auth = new JwtAuthenticationToken(mockCustomUser.userId(), null, authorities);
         context.setAuthentication(auth);
         return context;
     }
