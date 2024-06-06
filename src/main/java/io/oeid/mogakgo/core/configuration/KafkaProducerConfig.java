a8a16db67af877f6541d4ba96c8b1b9b146ddbd1
JIN-076 <andy230@naver.com>
2024-06-07 01:59:34 +0900

    [FIX] 주석 처리 및 ci 테스트

--- src/main/java/io/oeid/mogakgo/core/configuration/KafkaProducerConfig.java
+++ src/main/java/io/oeid/mogakgo/core/configuration/KafkaProducerConfig.java
@@ -11,7 +11,7 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
-/**
+/*
  * Deprecated annotation programming -> change to functional programming
  * @EnableBinding
  * @Input
