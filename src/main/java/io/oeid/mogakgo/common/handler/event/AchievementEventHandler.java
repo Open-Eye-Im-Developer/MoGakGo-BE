16763852eb952c0498458a942e45b5539850583b
JIN-076 <andy230@naver.com>
2024-06-12 02:59:00 +0900

    [FEAT] Transactional Outbox Pattern을 적용한 '이력 및 업적 처리' 관련 이벤트 핸들러 구현, Producer를 이용한 메시지 발행, 이벤트 발행 기록 로직 구현

---
+++ src/main/java/io/oeid/mogakgo/common/handler/event/AchievementEventHandler.java
@@ -0,0 +1,46 @@
+package io.oeid.mogakgo.common.handler.event;
+
+import io.oeid.mogakgo.core.properties.event.vo.AchievementEvent;
+import io.oeid.mogakgo.core.properties.kafka.MessageProducer;
+import io.oeid.mogakgo.domain.event.Event;
+import io.oeid.mogakgo.domain.outbox.domain.entity.OutboxEvent;
+import io.oeid.mogakgo.domain.outbox.infrastructure.OutboxJpaRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class AchievementEventHandler {
+
+    private static final String TOPIC = "Achievement";
+
+    private final OutboxJpaRepository outboxRepository;
+    private final MessageProducer messageProducer;
+
+    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
+    public void executeEvent(final AchievementEvent event) {
+
+        // TODO: 토픽에 어떤 메시지 형태로 컨슈머에게 전달할 것인지 고민
+        messageProducer.sendMessage(TOPIC, Event.builder()
+            .event(event)
+            .build()
+            .toString()
+        );
+
+        // TODO: Outbox를 조회할 식별자를 어떻게 구성할지 고민
+        OutboxEvent outbox = outboxRepository.findByKey(generateKey(event));
+        outbox.complete();
+
+    }
+
+    private String generateKey(final AchievementEvent event) {
+        return event.getUserId().toString() + event.getActivityType().toString();
+    }
+
+}
