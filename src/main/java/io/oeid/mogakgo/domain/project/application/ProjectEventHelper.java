da8941783dc46123401b4b0c23b74474959a2318
JIN-076 <andy230@naver.com>
2024-06-12 03:01:29 +0900

    [FEAT] 프로젝트 생성 관련 업적 처리를 위한 이벤트 헬퍼 클래스 리팩토링, Transactional Outbox Pattern 구현을 위한 이벤트 저장 로직 구현, 이벤트 발행 로직 구현

--- src/main/java/io/oeid/mogakgo/domain/project/application/ProjectEventHelper.java
+++ src/main/java/io/oeid/mogakgo/domain/project/application/ProjectEventHelper.java
@@ -1,14 +1,14 @@
 package io.oeid.mogakgo.domain.project.application;
 
-import io.oeid.mogakgo.common.event.domain.vo.AchievementCompletionEvent;
-import io.oeid.mogakgo.common.event.domain.vo.AchievementNotificationEvent;
-import io.oeid.mogakgo.common.event.domain.vo.UserActivityEvent;
+import io.oeid.mogakgo.core.properties.event.vo.AchievementEvent;
 import io.oeid.mogakgo.domain.achievement.domain.entity.enums.ActivityType;
+import io.oeid.mogakgo.domain.outbox.domain.EventType;
+import io.oeid.mogakgo.domain.outbox.domain.entity.OutboxEvent;
+import io.oeid.mogakgo.domain.outbox.infrastructure.OutboxJpaRepository;
 import io.oeid.mogakgo.domain.project.infrastructure.ProjectJpaRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,40 +21,34 @@ public class ProjectEventHelper {
 
     private static final int MAX_SERVICE_AREA = 26;
 
+    private final OutboxJpaRepository outboxRepository;
     private final ProjectJpaRepository projectRepository;
     private final ApplicationEventPublisher eventPublisher;
 
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void publishEvent(Long userId) {
 
         // -- '생성자' 프로젝트를 생성한 사용자에 대한 업적 이벤트 발행
+        // TODO: 발행해야 하는 여러 이벤트에 대해 병렬적으로 처리할 수 있을지 고민
         publishEvent(userId, ActivityType.PLEASE_GIVE_ME_MOGAK, null);
         publishEvent(userId, ActivityType.BRAVE_EXPLORER, checkCreatedProjectCountByRegion(userId));
     }
 
-    @Async("threadPoolTaskExecutor")
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void publishEvent(Long userId, ActivityType activityType, Object target) {
 
-        // -- 업적 이력에 대한 이벤트 발행
-        eventPublisher.publishEvent(UserActivityEvent.builder()
-            .userId(userId)
-            .activityType(activityType)
-            .build()
-        );
-
-        // -- 업적 달성 검증 및 처리에 대한 이벤트 발행
-        eventPublisher.publishEvent(AchievementCompletionEvent.builder()
+        // -- 업적 이력 및 달성 처리에 대한 이벤트 발행
+        eventPublisher.publishEvent(AchievementEvent.builder()
             .userId(userId)
             .activityType(activityType)
             .target(target)
             .build()
         );
 
-        // -- 업적 달성 알림에 대한 이벤트 발행
-        eventPublisher.publishEvent(AchievementNotificationEvent.builder()
-            .userId(userId)
-            .activityType(activityType)
-            .target(target)
+        // TODO: Outbox를 식별할 수 있는 식별자를 어떻게 구성할지 고민
+        outboxRepository.save(OutboxEvent.builder()
+            .type(EventType.ACHIEVEMENT)
+            .key(generateKey(userId, activityType))
             .build()
         );
     }
@@ -62,6 +56,11 @@ public class ProjectEventHelper {
     private Integer checkCreatedProjectCountByRegion(Long userId) {
         Integer progressCount = projectRepository.getRegionCountByUserId(userId);
         return progressCount.equals(MAX_SERVICE_AREA) ? 1 : 0;
+    }
+
+    // TODO: key값을 중복이 없는, 온전히 고유한 값으로 만들기 위한 요소가 필요
+    private String generateKey(Long userId, ActivityType activityType) {
+        return userId.toString() + activityType.toString();
     }
 
 }
