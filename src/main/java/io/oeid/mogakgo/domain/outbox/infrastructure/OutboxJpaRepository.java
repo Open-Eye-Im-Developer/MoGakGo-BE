72b94235de6f988d2195c53330696ec482ac3db6
JIN-076 <andy230@naver.com>
2024-06-13 00:17:16 +0900

    [FEAT] 이벤트 발행 요청 정보를 찾는 메서드 로직 수정

--- src/main/java/io/oeid/mogakgo/domain/outbox/infrastructure/OutboxJpaRepository.java
+++ src/main/java/io/oeid/mogakgo/domain/outbox/infrastructure/OutboxJpaRepository.java
@@ -1,11 +1,17 @@
 package io.oeid.mogakgo.domain.outbox.infrastructure;
 
 import io.oeid.mogakgo.domain.outbox.domain.entity.OutboxEvent;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OutboxJpaRepository extends JpaRepository<OutboxEvent, Long> {
 
-    OutboxEvent findByKey(String key);
+    @Query("""
+        select obe from OutboxEvent obe where obe.key = :key and obe.status = 'PENDING'
+        order by obe.createdAt desc limit 1
+    """)
+    Optional<OutboxEvent> findByKey(String key);
 }
