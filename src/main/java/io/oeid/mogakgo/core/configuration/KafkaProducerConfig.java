686557e745c1fba2bac3a880cc00ce377cbec8dc
JIN-076 <andy230@naver.com>
2024-06-15 01:45:09 +0900

    [FEAT] ProducerConfig에 ACK 설정 추가

--- src/main/java/io/oeid/mogakgo/core/configuration/KafkaProducerConfig.java
+++ src/main/java/io/oeid/mogakgo/core/configuration/KafkaProducerConfig.java
@@ -29,6 +29,9 @@ public class KafkaProducerConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String BOOTSTRAP_SERVERS;
 
+    @Value("${spring.kafka.producer.acks}")
+    private Integer ACKS;
+
     /**
      * BOOTSTRAP_SERVERS_CONFIG
      * producer가 처음으로 연결할 kafka broker의 위치 설정
@@ -45,6 +48,7 @@ public class KafkaProducerConfig {
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        props.put(ProducerConfig.ACKS_CONFIG, ACKS);
 
         return new DefaultKafkaProducerFactory<>(props);
     }
