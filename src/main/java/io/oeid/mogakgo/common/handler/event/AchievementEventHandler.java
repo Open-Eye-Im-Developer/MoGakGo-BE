ba5d78ae7ebf02a8ced8c9ca1f97ac3e6237cc2b
JIN-076 <andy230@naver.com>
2024-06-15 01:44:32 +0900

    [FEAT] 이벤트 발행 비동기 로직 완료 후에 자동으로 호출되는 성공 콜백으로 이벤트 요청 업데이트 로직 이동, 발행한 이벤트에 대해 별도의 스레드에서 처리하도록 @Async 추가

--- src/main/java/io/oeid/mogakgo/common/handler/event/AchievementEventHandler.java
+++ src/main/java/io/oeid/mogakgo/common/handler/event/AchievementEventHandler.java
@@ -1,16 +1,11 @@
 package io.oeid.mogakgo.common.handler.event;
 
-import static io.oeid.mogakgo.exception.code.ErrorCode404.OUTBOX_EVENT_NOT_FOUND;
-
 import io.oeid.mogakgo.core.properties.event.vo.AchievementEvent;
-import io.oeid.mogakgo.core.properties.event.vo.GeneralEvent;
 import io.oeid.mogakgo.core.properties.kafka.MessageProducer;
 import io.oeid.mogakgo.domain.event.Event;
-import io.oeid.mogakgo.domain.outbox.domain.entity.OutboxEvent;
-import io.oeid.mogakgo.domain.outbox.exception.OutboxException;
-import io.oeid.mogakgo.domain.outbox.infrastructure.OutboxJpaRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
@@ -24,12 +19,13 @@ public class AchievementEventHandler {
 
     private static final String TOPIC = "achievement";
 
-    private final OutboxJpaRepository outboxRepository;
     private final MessageProducer messageProducer;
 
+    @Async // 비동기로 호출, 즉 별도의 tx에서 처리
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void executeEvent(final AchievementEvent event) {
 
+        // SimpleAsyncTaskExecutor-1
         log.info("published event '{}' through thread '{}'", event, Thread.currentThread().getName());
 
         // TODO: 토픽에 어떤 메시지 형태로 컨슈머에게 전달할 것인지 고민
@@ -38,19 +34,6 @@ public class AchievementEventHandler {
             .build()
         );
 
-        // TODO: Outbox를 조회할 식별자를 어떻게 구성할지 고민✓
-        OutboxEvent outbox = getRequestedEvent(generateKey(event));
-        outbox.complete();
-
-    }
-
-    private OutboxEvent getRequestedEvent(String key) {
-        return outboxRepository.findByKey(key)
-            .orElseThrow(() -> new OutboxException(OUTBOX_EVENT_NOT_FOUND));
-    }
-
-    private String generateKey(final GeneralEvent event) {
-        return event.getUserId().toString() + "-" + event.getActivityType().toString();
     }
 
 }
--- src/main/java/io/oeid/mogakgo/core/properties/kafka/MessageProducer.java
+++ src/main/java/io/oeid/mogakgo/core/properties/kafka/MessageProducer.java
@@ -1,8 +1,17 @@
 package io.oeid.mogakgo.core.properties.kafka;
 
+import static io.oeid.mogakgo.exception.code.ErrorCode404.OUTBOX_EVENT_NOT_FOUND;
+
+import io.oeid.mogakgo.core.properties.event.vo.GeneralEvent;
+import io.oeid.mogakgo.domain.event.Event;
+import io.oeid.mogakgo.domain.outbox.domain.entity.OutboxEvent;
+import io.oeid.mogakgo.domain.outbox.exception.OutboxException;
+import io.oeid.mogakgo.domain.outbox.infrastructure.OutboxJpaRepository;
+import java.util.concurrent.ThreadPoolExecutor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -10,10 +19,56 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class MessageProducer {
 
-    private final KafkaTemplate<String, String> kafkaTemplate;
 
-    public void sendMessage(String topic, String message) {
-        kafkaTemplate.send(topic, message);
+    private final ThreadPoolExecutor executor;
+    private final KafkaTemplate<String, Event<?>> kafkaTemplate;
+    private final OutboxJpaRepository outboxRepository;
+
+    public void sendMessage(String topic, Event<?> event) {
+        kafkaTemplate.send(topic, event)
+            .whenComplete(
+                (res, ex) -> {
+                    if (ex != null) {
+                        // handle the exception scenario
+
+                        // TODO: 메시지 발행 실패에 대한 재처리 전략 구성 필요
+                        log.error("Failed to send message due to '{}'", ex.getMessage());
+                    } else if (res != null) {
+                        // send data to db
+
+                        log.info("Sent message '{}' through thread '{}'",
+                            event, Thread.currentThread().getName()); // kafka-producer-network-thread | producer-1
+                        log.info("Sent message '{}' with offset '{}' thorugh topic '{}'",
+                            event, res.getRecordMetadata().offset(), topic);
+
+                        // TODO: 디비 작업의 병목현상을 해결할 수 있는 방안 필요
+                        executor.execute(process(res));
+                    }
+                }
+            );
+    }
+
+    private Runnable process(SendResult<String, Event<?>> res) {
+        return () -> {
+            // TODO: Outbox를 조회할 식별자를 어떻게 구성할지 고민✓
+            OutboxEvent outbox = getRequestedEvent(
+                generateKey((GeneralEvent) res.getProducerRecord().value().getEvent())
+            );
+            outbox.complete();
+        };
+    }
+
+    private OutboxEvent getRequestedEvent(String key) {
+        return outboxRepository.findByKey(key)
+            .orElseThrow(() -> new OutboxException(OUTBOX_EVENT_NOT_FOUND));
+    }
+
+    private String generateKey(final GeneralEvent event) {
+        return event.getUserId().toString() + event.getActivityType().toString();
+    }
+
+    public void sendMessage(String topic, String key, Event<?> event) {
+        kafkaTemplate.send(topic, key, event);
     }
 
 }
