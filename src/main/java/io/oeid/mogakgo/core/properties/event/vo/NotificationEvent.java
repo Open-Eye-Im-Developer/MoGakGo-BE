94021930ca356c2b04145d37f1e2c757aed8c94e
JIN-076 <andy230@naver.com>
2024-06-18 01:24:47 +0900

    [FEAT] 메시지 엔티티에 식별자 추가

--- src/main/java/io/oeid/mogakgo/core/properties/event/vo/NotificationEvent.java
+++ src/main/java/io/oeid/mogakgo/core/properties/event/vo/NotificationEvent.java
@@ -1,5 +1,6 @@
 package io.oeid.mogakgo.core.properties.event.vo;
 
+import io.oeid.mogakgo.common.generator.UUIDGenerator;
 import io.oeid.mogakgo.domain.achievement.domain.entity.enums.ActivityType;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,11 +10,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class NotificationEvent extends GeneralEvent {
 
+    private String id;
     private Object target;
 
     @Builder
     private NotificationEvent(Long userId, ActivityType activityType, Object target) {
         super(userId, activityType);
+        this.id = UUIDGenerator.generateUUID();
         this.target = target;
     }
 }
