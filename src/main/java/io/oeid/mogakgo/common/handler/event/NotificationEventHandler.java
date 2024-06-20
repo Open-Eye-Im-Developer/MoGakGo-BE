4536d1f4f04843badae5d3f61f046f9698c4ca50
JIN-076 <andy230@naver.com>
2024-06-21 00:09:37 +0900

    [FEAT] 이벤트 수신 로직을 별도 트랜잭션으로 분리

--- src/main/java/io/oeid/mogakgo/common/handler/event/NotificationEventHandler.java
+++ src/main/java/io/oeid/mogakgo/common/handler/event/NotificationEventHandler.java
@@ -8,13 +8,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 @Slf4j
 @Service
-@Transactional("transactionManager")
+@Transactional(value = "transactionManager", propagation = Propagation.REQUIRES_NEW)
 @RequiredArgsConstructor
 public class NotificationEventHandler {
 
