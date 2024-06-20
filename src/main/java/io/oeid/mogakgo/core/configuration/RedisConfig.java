1e9d641d0bd32c623ed654bb540d1866557a62de
JIN-076 <andy230@naver.com>
2024-06-20 18:22:27 +0900

    [FEAT] Redis Config 추가

---
+++ src/main/java/io/oeid/mogakgo/core/configuration/RedisConfig.java
@@ -0,0 +1,24 @@
+package io.oeid.mogakgo.core.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    LettuceConnectionFactory connectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
+    }
+}
