53c291db51f38596356e471a3f0afa7d9c2bfcc1
JIN-076 <andy230@naver.com>
2024-06-22 04:00:55 +0900

    [FEAT] Cache Aside 전략 적용을 위한 로컬 캐시, 글로벌 캐시 구현, 로컬 캐시, 글로컬 캐시 순으로 조회하도록 로직 수정, 조회수에 기반한 TTL 증가 로직 구현, 디비에서 데이터를 미리 캐싱하는 CacheWarning 로직 구현,

--- src/main/java/io/oeid/mogakgo/domain/log/application/CacheService.java
+++ src/main/java/io/oeid/mogakgo/domain/log/application/CacheService.java
@@ -12,21 +12,52 @@ import org.springframework.stereotype.Service;
 public class CacheService {
 
     private static final String CACHE_KEY_PREFIX = "processed:";
+    private static final String HITS_COUNT_PREFIX = "hits:";
+    private static final Long INIT_TTL = 3600L;
+    private static final Long MAX_TTL = 86400L;
 
     private final RedisTemplate<String, Object> redisTemplate;
+    private final LocalCache localCache;
 
     public void cacheMessageId(String eventId) {
         String cacheKey = generateCacheKey(eventId);
-        redisTemplate.opsForValue().set(cacheKey, true, 1, TimeUnit.DAYS);
+        redisTemplate.opsForValue().set(cacheKey, true, INIT_TTL, TimeUnit.SECONDS);
+        localCache.put(cacheKey, "processed");
     }
 
+    // TODO: 조회 수(Hits)를 활용해 동적으로 TTL 증가 로직 구현
     public boolean isMessageIdCached(String eventId) {
         String cacheKey = generateCacheKey(eventId);
-        return Boolean.TRUE.equals(redisTemplate.hasKey(cacheKey));
+
+        if (localCache.containskey(cacheKey)) {
+            incrementHitsCount(eventId);
+            return true;
+        }
+
+        if (Boolean.TRUE.equals(redisTemplate.hasKey(cacheKey))) {
+            localCache.put(cacheKey, "processed");
+            incrementHitsCount(eventId);
+            return true;
+        }
+        return false;
     }
 
     public String generateCacheKey(String eventId) {
         return CACHE_KEY_PREFIX + eventId;
+    }
+
+    private void incrementHitsCount(String eventId) {
+        String hitsKey = HITS_COUNT_PREFIX + eventId;
+        Long hits = redisTemplate.opsForValue().increment(hitsKey);
+
+        if (hits != null && hits % 10 == 0) {
+            String cacheKey = generateCacheKey(eventId);
+            Long currentTTL = redisTemplate.getExpire(cacheKey, TimeUnit.SECONDS);
+            if (currentTTL != null && currentTTL < MAX_TTL) {
+                long newTTL = Math.min(currentTTL + 3600, MAX_TTL);
+                redisTemplate.expire(cacheKey, newTTL, TimeUnit.SECONDS);
+            }
+        }
     }
 
 }
