4e6e70d85d3606cb02f9b2414667a015922138ed
JIN-076 <andy230@naver.com>
2024-06-24 20:10:45 +0900

    [FEAT] JpaTxManager, KafkaTxManager @Bean으로 등록

--- src/main/java/io/oeid/mogakgo/core/configuration/JpaConfig.java
+++ src/main/java/io/oeid/mogakgo/core/configuration/JpaConfig.java
@@ -1,10 +1,22 @@
 package io.oeid.mogakgo.core.configuration;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableJpaAuditing
+@EnableTransactionManagement
+@RequiredArgsConstructor
 public class JpaConfig {
 
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new JpaTransactionManager();
+    }
 }
--- src/main/java/io/oeid/mogakgo/core/configuration/KafkaProducerConfig.java
+++ src/main/java/io/oeid/mogakgo/core/configuration/KafkaProducerConfig.java
@@ -12,6 +12,7 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.kafka.transaction.KafkaTransactionManager;
 
 /*
  * Deprecated annotation programming -> change to functional programming
@@ -81,6 +82,8 @@ public class KafkaProducerConfig {
         return new KafkaTemplate<>(producerFactory());
     }
 
-
-
+    @Bean
+    public KafkaTransactionManager<String, Event<?>> kafkaTransactionManager() {
+        return new KafkaTransactionManager<>(producerFactory());
+    }
 }
